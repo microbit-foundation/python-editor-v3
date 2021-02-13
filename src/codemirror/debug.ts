51d7a4a6641d14814dbedf957aa7d7f5970a852a
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-02-13 12:27:43 +0000

    Debug code as used in bug report.

---
+++ src/codemirror/debug.ts
@@ -0,0 +1,39 @@
+import { syntaxTree } from "@codemirror/language"
+import { NodeType } from "lezer-tree";
+import { EditorView, ViewPlugin, ViewUpdate } from "@codemirror/view"
+
+function isInteresting(type: NodeType) {
+  // Just to limit the output to something readable. Bug still evident with full tree.
+  return type.name === "ClassDefinition" || type.name === "FunctionDefinition";
+}
+
+export const debug = () => ViewPlugin.fromClass(class {
+  constructor(readonly view: EditorView) {
+  }
+  update(_update: ViewUpdate) {
+    this.view.requestMeasure({
+      read: () => {
+        let tree = syntaxTree(this.view.state);
+        let depth = 0;
+        console.log("Syntax tree dump:")
+        tree.iterate({from: 0, to: tree.length,
+          enter: (type, start) => {
+            if (isInteresting(type)) {
+              depth++;
+              let indent = "  ".repeat(depth);
+              console.log(`${indent} ${type.name} ${start}`)
+            }
+          },
+          leave: (type, _start, _end) => {
+            if (isInteresting(type)) {
+              depth--;
+            }
+          }
+        })
+      },
+      write: () => {}
+    })
+  }
+  destroy() {
+  }
+});
