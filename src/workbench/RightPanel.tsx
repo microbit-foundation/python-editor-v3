17755b5cf806f64aa912f097e2f0a24225ba7011
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-02-17 17:30:52 +0000

    Reinstate a right panel.
    Doesn't do anything useful, but interesting for discussion.

--- src/workbench/RightPanel.tsx
+++ src/workbench/RightPanel.tsx
@@ -4,12 +4,19 @@ import Placeholder from "../common/Placeholder";
 
 const RightPanel = () => {
   return (
-    <Tabs paddingTop={1} height="100%" variant="enclosed">
-      <TabList>
-        <Tab>Simulator</Tab>
-        <Tab>REPL</Tab>
+    <Tabs
+      paddingTop={1}
+      height="100%"
+      variant="enclosed"
+      backgroundColor="whitesmoke"
+      display="flex"
+      flexDirection="column"
+    >
+      <TabList backgroundColor="whitesmoke" flex="0 0 auto">
+        <Tab backgroundColor="white">Simulator</Tab>
+        <Tab backgroundColor="white">Serial</Tab>
       </TabList>
-      <TabPanels>
+      <TabPanels backgroundColor="white" flex="1 0 auto">
         <TabPanel>
           <Placeholder />
         </TabPanel>
