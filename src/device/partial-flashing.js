42d5a419a3b4690d92b38cd0a704f417d1b10448
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-03-02 19:37:25 +0000

    Faff with comments.

--- src/device/partial-flashing.js
+++ src/device/partial-flashing.js
@@ -1,40 +1,46 @@
+/**
+ * Implementation of partial flashing for the micro:bit.
+ *
+ * This could do with some love to make it easier to follow.
+ */
 import * as DAPjs from "dapjs";
 import * as PartialFlashingUtils from "./partial-flashing-utils";
 
-/*
-    This file is made up of a combination of original code, along with code extracted from the following repositories:
-    https://github.com/mmoskal/dapjs/tree/a32f11f54e9e76a9c61896ddd425c1cb1a29c143
-    https://github.com/microsoft/pxt-microbit
+// NOTICE
+//
+// This file is made up of a combination of original code, along with code
+// extracted from the following repositories:
+//
+// https://github.com/mmoskal/dapjs/tree/a32f11f54e9e76a9c61896ddd425c1cb1a29c143
+// https://github.com/microsoft/pxt-microbit
+//
+// The pxt-microbit license is included below.
 
-    The pxt-microbit license is included below.
-*/
-/*
-    PXT - Programming Experience Toolkit
-
-    The MIT License (MIT)
-
-    Copyright (c) Microsoft Corporation
-
-    All rights reserved.
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-*/
+// PXT - Programming Experience Toolkit
+//
+// The MIT License (MIT)
+//
+// Copyright (c) Microsoft Corporation
+//
+// All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 export class DAPWrapper {
   constructor(device) {
