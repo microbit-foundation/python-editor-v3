4faeec6447dcb1b0337da85815323fffc93e85af
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-03-03 17:37:46 +0000

    Simulator placeholder.

---
+++ src/workbench/Simulator.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+/**
+ * Not a simulator!
+ */
+const Simulator = () => {
+  return (
+    <img
+      style={{ width: "100%" }}
+      src="https://user-images.githubusercontent.com/44397098/109841157-af8df780-7c40-11eb-964f-8820206d0dbd.png"
+    />
+  );
+};
+
+export default Simulator;
