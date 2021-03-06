bce6496fd684c7b3f74da5a6255dd81530e74446
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-03-06 21:57:27 +0000

    WIP sketch of wiring.
    Next step reinstate serial.

--- src/serial/XTerm.tsx
+++ src/serial/XTerm.tsx
@@ -1,17 +1,26 @@
 import { useEffect, useRef } from "react";
 import { Terminal } from "xterm";
 import "xterm/css/xterm.css";
+import { EVENT_SERIAL_DATA } from "../device/device";
+import { useDevice } from "../device/device-hooks";
 
 const XTerm = () => {
+  const device = useDevice();
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (ref.current) {
       const terminal = new Terminal();
-      terminal.write("Data from serial written here");
-      terminal.onData((data) => console.log("Send this via serial", data));
       terminal.open(ref.current);
+
+      device.on(EVENT_SERIAL_DATA, (data) => {
+        terminal.write(data);
+      });
+      terminal.onData((data) => {
+        device.serialWrite(data);
+      });
+      // TODO: some kind of reset event?
     }
-  }, []);
+  }, [device]);
   return <div ref={ref} />;
 };
 
