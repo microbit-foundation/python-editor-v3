81eb08a23844a908cb9364e234572826d5c4bcea
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-03-09 18:41:17 +0000

    We want something like this.
    Needs to cope with it not being supported, and we might want to hide it
    entirely. I think it's for the best not to show it by default for the
    moment.

--- src/serial/SerialArea.tsx
+++ src/serial/SerialArea.tsx
@@ -1,12 +1,25 @@
 import { Flex } from "@chakra-ui/react";
+import Placeholder from "../common/Placeholder";
+import { ConnectionStatus } from "../device/device";
+import { useConnectionStatus } from "../device/device-hooks";
 import ProjectActionBar from "../project/ProjectActionBar";
 import XTerm from "./XTerm";
 
 const SerialArea = () => {
+  const connected = useConnectionStatus() === ConnectionStatus.CONNECTED;
   return (
     <Flex flexDirection="column" alignItems="stretch" height="100%">
       <ProjectActionBar mt={1} mb={1} ml={2} mr={2} />
-      <XTerm flex="1 1 auto" height={0} />
+      {connected ? (
+        <XTerm flex="1 1 auto" height={0} />
+      ) : (
+        <Placeholder
+          flex="1 1 auto"
+          backgroundColor="black"
+          color="white"
+          text="Connect via WebUSB to see serial output from your micro:bit here"
+        />
+      )}
     </Flex>
   );
 };
