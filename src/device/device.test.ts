db61b495a3387166bff0ef0770764b3499bd6c46
Matt Hillsdon <44397098+microbit-matt-hillsdon@users.noreply.github.com>
2021-03-11 13:17:15 +0000

    Initial WebUSB tests in nodejs mode with webusb providing nodejs based transport (#12)
    Browser automation (e.g. puppeteer) doesn't let us get past the dialog to choose a
    device, so test in node with the WebUSB API supplied by `webusb`.
    
    Disabled unless you set TEST_MODE_DEVICE to something truthy.
    
    Many stub cases to be filled out.

--- src/device/device.test.ts
+++ src/device/device.test.ts
@@ -5,14 +5,26 @@
  * It might be we could create a custom environment that was web but
  * with a tweak to Buffer.
  */
-import { ConnectionStatus, MicrobitWebUSBConnection } from "./device";
+import {
+  ConnectionStatus,
+  EVENT_STATUS,
+  MicrobitWebUSBConnection,
+} from "./device";
 import { USB } from "webusb";
 
 const describeDeviceOnly = process.env.TEST_MODE_DEVICE
   ? describe
   : describe.skip;
 
-describeDeviceOnly("MicrobitWebUSBConnection", () => {
+describe("MicrobitWebUSBConnection (WebUSB unsupported)", () => {
+  it("notices if WebUSB isn't supported", () => {
+    (global as any).navigator = {};
+    const microbit = new MicrobitWebUSBConnection();
+    expect(microbit.status).toBe(ConnectionStatus.NOT_SUPPORTED);
+  });
+});
+
+describeDeviceOnly("MicrobitWebUSBConnection (WebUSB supported)", () => {
   beforeAll(() => {
     // Maybe we can move this to a custom jest environment?
     (global as any).navigator = {
@@ -20,15 +32,61 @@ describeDeviceOnly("MicrobitWebUSBConnection", () => {
     };
   });
 
-  it(`should have correct initial state`, () => {
+  it("shows no device as initial status", () => {
     const microbit = new MicrobitWebUSBConnection();
     expect(microbit.status).toBe(ConnectionStatus.NO_AUTHORIZED_DEVICE);
   });
-  it("connects", async () => {
+
+  it("connects and disconnects updating status and events", async () => {
+    const events: ConnectionStatus[] = [];
     const connection = new MicrobitWebUSBConnection();
+    connection.on(EVENT_STATUS, (status: ConnectionStatus) =>
+      events.push(status)
+    );
+
     await connection.connect();
+
+    expect(connection.status).toEqual(ConnectionStatus.CONNECTED);
+    expect(events).toEqual([ConnectionStatus.CONNECTED]);
+
     // without this it breaks! something is up!
     await new Promise((resolve) => setTimeout(resolve, 100));
     await connection.disconnect();
+    connection.dispose();
+
+    expect(connection.status).toEqual(ConnectionStatus.NOT_CONNECTED);
+    expect(events).toEqual([
+      ConnectionStatus.CONNECTED,
+      ConnectionStatus.NOT_CONNECTED,
+    ]);
   });
+
+  it("flashes using partial flashing when possible", async () => {
+    // Flash MakeCode hex that outputs serial.
+    // Flash MicroPython hex that outputs serial.
+    // Flash another MicroPython hex, assert that we got a small number of progress events.
+    // I think we need to rejig the interface to get flash data before writing this.
+  });
+
+  it("connects to flash and stays connected afterwards", () => {
+    // Is this actually sensible?
+  });
+
+  it("fires a serial reset event when stopping serial", () => {});
+
+  it("reinstates serial after flashing", () => {});
+
+  it("serial echo test", async () => {
+    // Flash a hex that echos serial
+    // Use serialWrite to write serial data.
+    // Assert same content is received.
+    // Useful to test unicode?
+  });
+
+  it("notices disconnects and updates status", () => {
+    // Might need a mock-style tests for this.
+  });
+
+  // Is it feasible to test many of the error cases?
+  // I think we'd need to pass a mock DAPWrapper.
 });
