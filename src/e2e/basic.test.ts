c409334c4e3f66bf16e0f580ab283194c35b1b86
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-03-15 16:58:03 +0000

    Refactor the test to model the page.

--- src/e2e/basic.test.ts
+++ src/e2e/basic.test.ts
@@ -1,23 +1,12 @@
-import puppeteer from "puppeteer";
-import "pptr-testing-library/extend";
+import { App } from "./app";
 
 describe("Toolbar actions", () => {
-  it("Download - downloads a HEX file", async () => {
-    const browser = await puppeteer.launch();
+  const app = new App();
 
-    const page = await browser.newPage();
-    await page.goto("http://localhost:3000");
-    const client = await page.target().createCDPSession();
-    await client.send("Page.setDownloadBehavior", {
-      behavior: "allow",
-      downloadPath: "/tmp",
-    });
+  beforeAll(app.reload.bind(app));
+  afterAll(app.dispose.bind(app));
 
-    const document = await page.getDocument();
-    const downloadButton = await document.getByText("Download");
-    await downloadButton.click();
-    // TODO: wait for download, check file
-
-    browser.close();
+  it("Download - download the default HEX file", async () => {
+    await app.download();
   });
 });
