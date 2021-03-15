48e62959a13f757bc92022caf4f4f8a1ac7abb6a
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-03-15 09:03:46 +0000

    First stab at CircleCI build.

---
+++ deployment.js
@@ -0,0 +1,16 @@
+const createDeploymentDetails = require("@microbit-foundation/website-deploy-aws-config");
+
+const { s3Config } = createDeploymentDetails();
+module.exports = {
+  ...s3Config,
+  region: "eu-west-1",
+  removeNonexistentObjects: true,
+  enableS3StaticWebsiteHosting: true,
+  errorDocumentKey: "index.html",
+  redirects: [],
+  params: {
+    "static/**": { CacheControl: "public, max-age=31536000, immutable" },
+    "**/**/!(sw).js": { CacheControl: "public, max-age=31536000, immutable" },
+    "**/**.css": { CacheControl: "public, max-age=31536000, immutable" },
+  },
+};
---
+++ src/e2e/basic.test.ts
@@ -0,0 +1,23 @@
+import puppeteer from "puppeteer";
+import "pptr-testing-library/extend";
+
+describe("Toolbar actions", () => {
+  it("Download - downloads a HEX file", async () => {
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+    await page.goto("http://localhost:5000");
+    const client = await page.target().createCDPSession();
+    await client.send("Page.setDownloadBehavior", {
+      behavior: "allow",
+      downloadPath: "/tmp",
+    });
+
+    const document = await page.getDocument();
+    const downloadButton = await document.getByText("Download");
+    await downloadButton.click();
+    // TODO: wait for download, check file
+
+    browser.close();
+  });
+});
