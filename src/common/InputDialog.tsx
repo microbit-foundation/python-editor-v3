2272bd6836f877de5cd164ede2036638a3f93414
Matt Hillsdon <44397098+microbit-matt-hillsdon@users.noreply.github.com>
2021-03-23 10:12:42 +0000

    Collect a filename for create file. (#39)
    The input dialog is currently really a filename dialog, but there's
    plenty of scope to generalise it to take a form component which gets its
    state passed in, validated, then returned.
    
    Add an e2e test for file creation.
    
    https://github.com/microbit-foundation/python-editor-next/issues/21

---
+++ src/common/InputDialog.tsx
@@ -0,0 +1,102 @@
+import { Button } from "@chakra-ui/button";
+import {
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+} from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { VStack } from "@chakra-ui/layout";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/modal";
+import { ReactNode, useRef, useState } from "react";
+
+export interface InputDialogParameters {
+  header: ReactNode;
+  body: ReactNode;
+  actionLabel: string;
+  validate: (input: string) => string | undefined;
+}
+
+export interface InputDialogParametersWithActions
+  extends InputDialogParameters {
+  header: ReactNode;
+  body: ReactNode;
+  actionLabel: string;
+  onConfirm: (validValue: string) => void;
+  onCancel: () => void;
+}
+
+export interface InputDialogProps extends InputDialogParametersWithActions {
+  isOpen: boolean;
+}
+
+/**
+ * File name input dialog.
+ *
+ * Generally not used directly. Prefer the useDialogs hook.
+ */
+export const InputDialog = ({
+  header,
+  body,
+  actionLabel,
+  isOpen,
+  validate,
+  onConfirm,
+  onCancel,
+}: InputDialogProps) => {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState<string | undefined>(undefined);
+  const leastDestructiveRef = useRef<HTMLButtonElement>(null);
+  return (
+    <Modal isOpen={isOpen} onClose={onCancel}>
+      <ModalOverlay>
+        <ModalContent>
+          <ModalHeader fontSize="lg" fontWeight="bold">
+            {header}
+          </ModalHeader>
+          <ModalBody>
+            <VStack>
+              {body}
+              <FormControl id="fileName" isRequired isInvalid={Boolean(error)}>
+                <FormLabel>Name</FormLabel>
+                <Input
+                  type="text"
+                  value={value}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setValue(value);
+                    setError(validate(value));
+                  }}
+                ></Input>
+                <FormHelperText>
+                  We'll add the ".py" extension for you.
+                </FormHelperText>
+                <FormErrorMessage>{error}</FormErrorMessage>
+              </FormControl>
+            </VStack>
+          </ModalBody>
+          <ModalFooter>
+            <Button ref={leastDestructiveRef} onClick={onCancel}>
+              Cancel
+            </Button>
+            <Button
+              colorScheme="blue"
+              onClick={() => onConfirm(value)}
+              ml={3}
+              isDisabled={Boolean(error)}
+            >
+              {actionLabel}
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </ModalOverlay>
+    </Modal>
+  );
+};
