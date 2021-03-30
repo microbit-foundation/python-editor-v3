5266e0228490b9e0a79f5b5db7a6e85204bd353b
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-03-30 11:49:24 +0100

    Width/height/font size for ace.

--- src/editor/ace/Ace.tsx
+++ src/editor/ace/Ace.tsx
@@ -5,11 +5,17 @@ import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/ext-language_tools";
 import { EditorComponentProps } from "../editor";
 
-const Ace = ({ defaultValue, onChange }: EditorComponentProps) => {
+const ptToPixelRatio = 96 / 72;
+
+const Ace = ({ defaultValue, onChange, fontSize }: EditorComponentProps) => {
   return (
     <AceEditor
       mode="python"
       theme="github"
+      tabSize={4}
+      height="100%"
+      width="100%"
+      fontSize={fontSize * ptToPixelRatio}
       defaultValue={defaultValue}
       onChange={onChange}
       name="ace-editor"
