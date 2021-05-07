06fba1e35d851d326977929e6178d62347ba2f06
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-05-07 15:00:32 +0100

    Need to skip these more fundamentally in CI.

--- src/e2e/flash.test.ts
+++ src/e2e/flash.test.ts
@@ -3,21 +3,19 @@ import { App, BrowserMode } from "./app";
 // Allow tests to perform full flashes.
 jest.setTimeout(60_000);
 
-const describeDeviceOnly = process.env.TEST_MODE_DEVICE
-  ? describe
-  : describe.skip;
+if (process.env.TEST_MODE_DEVICE) {
+  describe("Browser - flash", () => {
+    const app = new App(BrowserMode.NORMAL_WITH_WEBUSB);
+    beforeEach(app.reset.bind(app));
+    afterAll(app.dispose.bind(app));
 
-describeDeviceOnly("Browser - flash", () => {
-  const app = new App(BrowserMode.NORMAL_WITH_WEBUSB);
-  beforeEach(app.reset.bind(app));
-  afterAll(app.dispose.bind(app));
-
-  it("connects, flashes, disconnects", async () => {
-    await app.connect();
-    try {
-      await app.flash();
-    } finally {
-      await app.disconnect();
-    }
+    it("connects, flashes, disconnects", async () => {
+      await app.connect();
+      try {
+        await app.flash();
+      } finally {
+        await app.disconnect();
+      }
+    });
   });
-});
+}
