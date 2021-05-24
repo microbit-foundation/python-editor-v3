dc40025a8bdc16195f7450725ab27ef6c793d4f9
Matt Hillsdon <44397098+microbit-matt-hillsdon@users.noreply.github.com>
2021-05-24 16:44:26 +0100

    Explain what we learnt

--- src/editor/codemirror/config.ts
+++ src/editor/codemirror/config.ts
@@ -73,6 +73,14 @@ export const themeExtensions = (fontSizePt: number) => {
   });
 };
 
+// CodeMirror maps grammar nodes to a set of predefined tags so that themes
+// can be written in a language independent way. Tags can extend others so it
+// is sufficient to address just the base tags in a theme but you can be more
+// specific if you need to.
+// This file defines all the tags and has useful documentation:
+// https://github.com/codemirror/highlight/blob/main/src/highlight.ts#L480
+// This file shows the mapping of grammar nodes to tags for Python
+// https://github.com/codemirror/lang-python/blob/main/src/python.ts#L17
 export const myHighlightStyle = () => {
   const dark = "var(--chakra-colors-gray-800)";
   return HighlightStyle.define([
