35639e3c91a9e9047e66c5f3a5aa48b46c1393d7
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-05-27 09:49:32 +0100

    Theme Alert as used in toasts.
    Background colour for success is TBC. The design suggested one has very
    dubious contrast with white text.

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -123,6 +123,23 @@ const overrides = {
         },
       },
     },
+    Alert: {
+      variants: {
+        toast: (props: any) => {
+          const base = {
+            ...theme.components.Alert.variants["solid"](props),
+          };
+          return {
+            ...base,
+            container: {
+              ...base.container,
+              // Designs say blimpTeal.50 but way too light for white text.
+              bg: props.status === "success" ? "blimpTeal.400" : "blimpPink.500"
+            }
+          };
+        },
+      },
+    },
   },
 };
 
