9c7d27900fef333eb5a0159f17e5831e8665b4a6
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-06-09 15:49:19 +0100

    Add missing lighter color that we need.

--- src/brand/default/colors.ts
+++ src/brand/default/colors.ts
@@ -1,7 +1,13 @@
 import { theme } from "@chakra-ui/theme";
 
+const gray = {
+  10: "#fcfcfc",
+  ...theme.colors.gray,
+};
+
 const colors = {
-  brand: theme.colors.gray,
+  brand: gray,
+  gray,
 };
 
 export default colors;
