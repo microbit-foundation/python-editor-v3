db6239a5c246e8c5a6de37e75c06235b3acfc777
Matt Hillsdon <44397098+microbit-matt-hillsdon@users.noreply.github.com>
2021-06-11 11:53:29 +0100

    Optionally use external project for theme etc. (#151)
    This commit introduces some indirection via a webpack alias to allow us to have a single import of @deployment that is switched at build time to use an external module containing brand assets or an internal default one.
    
    We use https://github.com/gsoft-inc/craco to allow webpack config customisation as CRA doesn't permit it by default. This carries some risk of slower CRA upgrades in future but I think we need something like this to get a plugin-like feature.
    
    I've gone with an import directly from a "scope" as it can't really be confused with a real NPM package.

--- src/deployment/microbit/Logo.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactComponent as Logo } from "./logo.svg";
-
-/**
- * The micro:bit logo.
- */
-export default Logo;
--- src/deployment/microbit/radii.ts
+++
@@ -1,6 +0,0 @@
-const radii = {
-  // Design radius for buttons and other larger items
-  "4xl": "2rem",
-};
-
-export default radii;
