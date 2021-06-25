36c213ab9d365b7368402545e84ceb2f1680a416
Matt Hillsdon <44397098+microbit-matt-hillsdon@users.noreply.github.com>
2021-06-25 14:27:11 +0100

    About dialog content. (#161)
    Adapted from Jonny's design. Expect further changes after feedback next week.

--- src/project/AboutDialog.tsx
+++ src/project/AboutDialog.tsx
@@ -1,21 +1,44 @@
 import { Button } from "@chakra-ui/button";
 import { useClipboard } from "@chakra-ui/hooks";
-import { HStack, Text, VStack } from "@chakra-ui/layout";
+import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
+import { Image } from "@chakra-ui/image";
+import {
+  Flex,
+  Link,
+  SimpleGrid,
+  Text,
+  VStack,
+  HStack,
+  BoxProps,
+} from "@chakra-ui/layout";
 import {
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
-  ModalHeader,
+  ModalFooter,
   ModalOverlay,
 } from "@chakra-ui/modal";
+import {
+  Collapse,
+  Table,
+  TableCaption,
+  Td,
+  Tr,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { RiFileCopy2Line } from "react-icons/ri";
+import { useDeployment } from "../deployment";
 import { microPythonVersions } from "../fs/micropython";
+import comicImage from "./comic.png";
+import microbitHeartImage from "./microbit-heart.png";
+import micropythonLogo from "./micropython.jpeg";
+import pythonPoweredLogo from "./python-powered.png";
 
 const versionInfo = [
-  { name: "Editor version", value: process.env.REACT_APP_VERSION },
+  { name: "Editor", value: process.env.REACT_APP_VERSION },
   {
-    name: "MicroPython version",
+    name: "MicroPython",
     value: microPythonVersions.map((mpy) => mpy.version).join("/"),
   },
 ];
@@ -30,43 +53,171 @@ interface AboutDialogProps {
 
 const AboutDialog = ({ isOpen, onClose }: AboutDialogProps) => {
   const { hasCopied, onCopy } = useClipboard(clipboardVersion);
+  const deployment = useDeployment();
+  const micropythonSection = useDisclosure();
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="2xl">
+    <Modal isOpen={isOpen} onClose={onClose} size="4xl">
       <ModalOverlay>
         <ModalContent>
-          <ModalHeader>About</ModalHeader>
+          <ModalCloseButton />
           <ModalBody>
-            <ModalCloseButton />
-            <VStack spacing={8} padding={5}>
-              <Text fontSize="lg" textAlign="center">
-                Made with love by the Micro:bit Educational Foundation and
-                contributors
-              </Text>
-              <HStack
-                alignSelf="flex-start"
-                alignItems="flex-start"
-                spacing={8}
-              >
-                <VStack alignItems="flex-start">
-                  {versionInfo.map((v) => (
-                    <Text key={v.name}>
-                      <Text as="span" fontWeight="semibold">
-                        {v.name}:
-                      </Text>{" "}
-                      {v.value}
-                    </Text>
-                  ))}
-                </VStack>
-                <Button icon={<RiFileCopy2Line />} onClick={onCopy}>
-                  {hasCopied ? "Copied" : "Copy"}
-                </Button>
+            <VStack spacing={8} pl={5} pr={5} pt={5}>
+              <HStack spacing={4}>
+                {deployment.horizontalLogo && (
+                  <Flex
+                    alignItems="center"
+                    justifyContent="flex-end"
+                    width="200px"
+                    mr={4}
+                  >
+                    {deployment.horizontalLogo}
+                  </Flex>
+                )}
+                <Flex alignItems="center" justifyContent="flex-end">
+                  <Image src={micropythonLogo} alt="MicroPython" />
+                </Flex>
+                <Flex
+                  alignItems="center"
+                  justifyContent="flex-end"
+                  alt="Python powered"
+                >
+                  <Image src={pythonPoweredLogo} />
+                </Flex>
               </HStack>
+
+              <Text fontSize="lg" textAlign="center">
+                Made with love by the{" "}
+                <Link
+                  color="brand.500"
+                  href="https://github.com/microbit-foundation/python-editor-next/graphs/contributors"
+                >
+                  Micro:bit Educational Foundation and contributors{" "}
+                </Link>
+              </Text>
+              <SimpleGrid columns={[1, 2, 2, 2]} spacing={8}>
+                <Image
+                  src={microbitHeartImage}
+                  sx={{ aspectRatio: 690 / 562 }}
+                  alt="micro:bit board with the 5 by 5 LED grid showing a heart"
+                />
+                <VStack
+                  alignItems="center"
+                  justifyContent="center"
+                  height="100%"
+                  spacing={4}
+                >
+                  <Table size="sm">
+                    {versionInfo.map((v) => (
+                      <Tr key={v.name}>
+                        <Td>{v.name}</Td>
+                        <Td>{v.value}</Td>
+                      </Tr>
+                    ))}
+                    <TableCaption color="gray.800" placement="top">
+                      Software versions
+                    </TableCaption>
+                  </Table>
+                  <Button
+                    leftIcon={<RiFileCopy2Line />}
+                    onClick={onCopy}
+                    size="md"
+                  >
+                    {hasCopied ? "Copied" : "Copy"}
+                  </Button>
+                </VStack>
+              </SimpleGrid>
+              <Text fontSize="lg">
+                The editor depends on{" "}
+                <Link
+                  color="brand.500"
+                  href="https://micropython.org"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  MicroPython
+                </Link>{" "}
+                which is made by Damien George and a community of developers
+                around the world.{" "}
+                <Button
+                  aria-label={
+                    micropythonSection.isOpen
+                      ? "Read less about MicroPython"
+                      : "Read more about MicroPython"
+                  }
+                  variant="unstyled"
+                  height="unset"
+                  verticalAlign="unset"
+                  fontSize="lg"
+                  fontWeight="normal"
+                  rightIcon={
+                    micropythonSection.isOpen ? (
+                      <ChevronUpIcon />
+                    ) : (
+                      <ChevronDownIcon />
+                    )
+                  }
+                  onClick={micropythonSection.onToggle}
+                >
+                  {micropythonSection.isOpen ? "Read less" : "Read more"}
+                </Button>
+              </Text>
             </VStack>
+            <Collapse in={micropythonSection.isOpen}>
+              {/* Avoid stack spacing here but match space so it doesn't change after the animation */}
+              <MicroPythonSection mt={8} pl={5} pr={5} />
+            </Collapse>
           </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose} variant="solid" size="lg">
+              Close
+            </Button>
+          </ModalFooter>
         </ModalContent>
       </ModalOverlay>
     </Modal>
   );
 };
+
+const MicroPythonSection = (props: BoxProps) => (
+  <VStack spacing={4} {...props}>
+    <Image
+      src={comicImage}
+      sx={{ aspectRatio: 1035 / 423 }}
+      alt={`Three panel comic titled "MicroPython Rocks" by Mike Rowbit. A cartoon snake introduces Damien, saying "Meet Damien... He created MicroPython.". Two snakes discuss MicroPython. The yellow snake says "MicroPython is designed to work on very small computers." "Like the BBC micro:bit" the purple snake replies." The yellows snake continues "But Python can run anywhere.". The purple snake agrees, saying "Like on this rack of severs that run huge websites". The background behind the snakes shows a server rack.`}
+    />
+    <SimpleGrid columns={[1, 1, 1, 2]} spacing={4} textAlign="center">
+      <Text fontSize="md">
+        MicroPython{" "}
+        <Link
+          color="brand.500"
+          href="https://github.com/bbcmicrobit/micropython"
+          target="_blank"
+          rel="noopener"
+        >
+          source code for the micro:bit V1
+        </Link>{" "}
+        and{" "}
+        <Link
+          color="brand.500"
+          href="https://github.com/microbit-foundation/micropython-microbit-v2"
+          target="_blank"
+          rel="noopener"
+        >
+          micro:bit V2
+        </Link>{" "}
+      </Text>
+      <Text fontSize="md">
+        <Link
+          color="brand.500"
+          href="https://ntoll.org/article/story-micropython-on-microbit/"
+          target="_blank"
+          rel="noopener"
+        >
+          Learn how MicroPython on the micro:bit came to be{" "}
+        </Link>
+      </Text>
+    </SimpleGrid>
+  </VStack>
+);
 
 export default AboutDialog;
