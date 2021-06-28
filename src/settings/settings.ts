c01e9f572ff5c8803024daf721a3986ed335eae6
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-06-28 11:56:47 +0100

    Limit when we add the test language.

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,5 +1,6 @@
 import { createContext, useContext } from "react";
 import { defaultCodeFontSizePt } from "../deployment/misc";
+import { stage } from "../environment";
 
 export interface Language {
   id: string;
@@ -12,7 +13,7 @@ export const supportedLanguages = [
     name: "English",
   },
 ];
-if (process.env.NODE_ENV !== "production") {
+if (stage === "REVIEW" || process.env.NODE_ENV !== "production") {
   supportedLanguages.push({
     id: "lol",
     name: "Translation test",
