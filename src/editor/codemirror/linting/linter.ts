e460a925f6dcac8848b02342e7c78c8edc9e46bd
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-07-28 15:42:12 +0100

    Switch to postion -> end of line underline.
    I think it's easy to understand whole-of-line so better to demo this.

--- src/editor/codemirror/linting/linter.ts
+++ src/editor/codemirror/linting/linter.ts
@@ -1,6 +1,5 @@
-import { linter, Diagnostic } from "@codemirror/lint";
+import { Diagnostic, linter } from "@codemirror/lint";
 import { EditorView } from "@codemirror/view";
-
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import Worker from "worker-loader!./linter.worker";
 
@@ -15,14 +14,18 @@ export const tygerPythonLinter = () => {
         (e: MessageEvent) => {
           const errors = e.data;
           resolve(
-            errors.map((error: any) => ({
-              severity: "error",
-              // We have an offset but hard to display clearly.
-              from: view.state.doc.line(error.line + 1).from,
-              to: view.state.doc.line(error.line + 1).to,
-              source: "TygerPython",
-              message: error.msg,
-            }))
+            errors.map((error: any) => {
+              return {
+                severity: "error",
+                // So that the go-to-next error feature works we highlight
+                // from the offset to the end of the line (as we don't have
+                // an end position). Not great when the error is at the end
+                // of a line.
+                from: view.state.doc.line(error.line + 1).from + error.offset,
+                to: view.state.doc.line(error.line + 1).to,
+                message: error.msg,
+              };
+            })
           );
         },
         { once: true }
