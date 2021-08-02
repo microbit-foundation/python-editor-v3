e05d59f4cdb921dfa47acd5ad1196c52d12bbd2e
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-08-02 18:15:27 +0100

    Check all keys.
    Use code so we match Ctrl+D and Ctrl+Shift+D as MicroPython doesn't care
    about shift.

--- src/serial/xterm-keyboard.ts
+++ src/serial/xterm-keyboard.ts
@@ -1,6 +1,5 @@
 const copyShortcut = (e: KeyboardEvent): boolean => {
-  console.log(e.key);
-  if (e.ctrlKey && e.shiftKey && e.key === "C") {
+  if (e.ctrlKey && e.shiftKey && e.code === "KeyC") {
     e.preventDefault();
     document.execCommand("copy");
     return false;
@@ -8,17 +7,30 @@ const copyShortcut = (e: KeyboardEvent): boolean => {
   return true;
 };
 
-const softRebootShortcut = (e: KeyboardEvent) => {
-  if (e.ctrlKey && e.key === "d") {
-    // MicroPython handles this as a soft reboot.
-    // Avoid it also adding a bookmark! (Ctrl-D on Windows/Linux)
+const isMicroPythonCtrlShortcut = (code: string) => {
+  // Editing keys are here: https://github.com/micropython/micropython/blob/46a11028521425e7d6c85458c849bb96ff82152e/lib/mp-readline/readline.c
+  switch (code) {
+    case "KeyA":
+    case "KeyB":
+    case "KeyC":
+    case "KeyD":
+    case "KeyE":
+      return true;
+    default:
+      return false;
+  }
+};
+
+const micropythonShortcuts = (e: KeyboardEvent) => {
+  if (e.ctrlKey && isMicroPythonCtrlShortcut(e.code)) {
+    // Avoid also triggering browser shortcuts, e.g. bookmark (Ctrl+D), search (Ctrl+E).
     e.preventDefault();
     e.stopPropagation();
   }
   return true;
 };
 
-const handlers = [copyShortcut, softRebootShortcut];
+const handlers = [copyShortcut, micropythonShortcuts];
 const customKeyEventHandler = (e: KeyboardEvent): boolean => {
   for (const handler of handlers) {
     if (!handler(e)) {
