9b359a3e0a71d366efb95f9ec642fb967079e8aa
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-08-25 10:32:57 +0100

    Avoid disconnect during flash.
    Fixes logical bug that I can no longer reproduce in practice.
    
    See https://github.com/microbit-foundation/python-editor-next/issues/223

--- src/device/device.ts
+++ src/device/device.ts
@@ -203,6 +203,8 @@ export class MicrobitWebUSBConnection
     this.emit(EVENT_SERIAL_DATA, data);
   };
 
+  private flashing: boolean = false;
+  private disconnectAfterFlash: boolean = false;
   private visibilityReconnect: boolean = false;
   private visibilityChangeListener = () => {
     if (document.visibilityState === "visible") {
@@ -210,14 +212,21 @@ export class MicrobitWebUSBConnection
         this.visibilityReconnect &&
         this.status !== ConnectionStatus.CONNECTED
       ) {
+        this.disconnectAfterFlash = false;
         this.visibilityReconnect = false;
-        this.connect();
+        if (!this.flashing) {
+          this.connect();
+        }
       }
     } else {
       if (!this.unloading && this.status === ConnectionStatus.CONNECTED) {
-        this.disconnect().then(() => {
-          this.visibilityReconnect = true;
-        });
+        if (!this.flashing) {
+          this.disconnect().then(() => {
+            this.visibilityReconnect = true;
+          });
+        } else {
+          this.disconnectAfterFlash = true;
+        }
       }
     }
   };
@@ -311,20 +320,26 @@ export class MicrobitWebUSBConnection
       progress: (percentage: number | undefined) => void;
     }
   ): Promise<void> {
-    const startTime = new Date().getTime();
+    this.flashing = true;
+    try {
+      const startTime = new Date().getTime();
 
-    await this.withEnrichedErrors(() =>
-      this.flashInternal(dataSource, options)
-    );
+      await this.withEnrichedErrors(() =>
+        this.flashInternal(dataSource, options)
+      );
 
-    const flashTime = new Date().getTime() - startTime;
-    this.logging.event({
-      type: "WebUSB-time",
-      detail: {
-        flashTime,
-      },
-    });
-    this.logging.log("Flash complete");
+      const flashTime = new Date().getTime() - startTime;
+      this.logging.event({
+        type: "WebUSB-time",
+        detail: {
+          flashTime,
+        },
+      });
+      this.logging.log("Flash complete");
+    }
+    finally {
+      this.flashing = false;
+    }
   }
 
   private async flashInternal(
@@ -356,12 +371,17 @@ export class MicrobitWebUSBConnection
     } finally {
       progress(undefined);
 
-      // This might not strictly be "reinstating". We should make this
-      // behaviour configurable when pulling out a library.
-      this.log("Reinstating serial after flash");
-      if (this.connection.daplink) {
-        await this.connection.daplink.connect();
-        await this.startSerialInternal();
+      if (this.disconnectAfterFlash) {
+        this.disconnectAfterFlash = false;
+        await this.disconnect();
+      } else {
+        // This might not strictly be "reinstating". We should make this
+        // behaviour configurable when pulling out a library.
+        this.log("Reinstating serial after flash");
+        if (this.connection.daplink) {
+          await this.connection.daplink.connect();
+          await this.startSerialInternal();
+        }
       }
     }
   }
