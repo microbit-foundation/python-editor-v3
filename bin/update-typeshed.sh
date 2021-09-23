9b8416fec15353d28e06bd0e6aa92f691421f23b
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-09-23 15:24:02 +0100

    Docs fixes in typeshed.
    Add script to aid updating.

---
+++ bin/update-typeshed.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# Utility to update the JSON file that stores the custom typeshed.
+#
+# Assumes it is run from the root of the editor project and the stubs project is a sibling checkout.
+#
+set -euxo pipefail
+(cd ../micropython-microbit-stubs && python3 browser-package.py)
+cp ../micropython-microbit-stubs/typeshed.json src/language-server/typeshed.json
