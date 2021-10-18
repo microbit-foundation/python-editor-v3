4ca4d693c7ff1fe611be0b276ccaf99dc413ebba
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-10-18 13:37:00 +0100

    Remove unused CSS.

--- src/editor/codemirror/language-server/documentation.ts
+++ src/editor/codemirror/language-server/documentation.ts
@@ -92,7 +92,8 @@ export const wrapWithDocumentationButton = (child: Element, id: string) => {
   const button = child.appendChild(document.createElement("button"));
   button.innerHTML =
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1rem" height="1rem"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zM11 7h2v2h-2V7zm0 4h2v6h-2v-6z"/></svg>';
-  button.ariaLabel = "More info";
+  // TODO: How do we get translations here?
+  button.ariaLabel = "Show more";
   button.onclick = () => {
     document.dispatchEvent(
       new CustomEvent("cm/openDocs", {
@@ -102,12 +103,10 @@ export const wrapWithDocumentationButton = (child: Element, id: string) => {
       })
     );
   };
-  button.style.marginTop = "auto";
   button.style.display = "block";
-  button.style.float = "right";
   button.style.margin = "0";
-  button.style.paddingTop = "0.5rem";
-  button.style.paddingBottom = "0.5rem";
+  button.style.marginRight = "-0.5rem";
+  button.style.padding = "0.5rem";
   button.style.alignSelf = "flex-end";
 
   wrapper.appendChild(button);
