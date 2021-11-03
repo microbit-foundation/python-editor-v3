b3cbc37fad229a500c1961cff0246f8531df3f5b
Matt Hillsdon <44397098+microbit-matt-hillsdon@users.noreply.github.com>
2021-11-03 10:19:38 +0000

    Fix missing licensing headers. (#306)
    Add a script to automate this.

---
+++ bin/fix-licensing-headers.js
@@ -0,0 +1,40 @@
+/**
+ * Script to add missing licensing headers.
+ * Usually run via npm script.
+ */
+const { promises: fsp } = require("fs");
+const path = require("path");
+
+const header = `/**
+ * (c) 2021, Micro:bit Educational Foundation and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+`;
+
+async function* walk(dir) {
+  for await (const d of await fsp.opendir(dir)) {
+    const entry = path.join(dir, d.name);
+    if (d.isDirectory()) {
+      yield* walk(entry);
+    } else if (d.isFile()) {
+      yield entry;
+    }
+  }
+}
+
+async function main() {
+  for await (const p of walk("src")) {
+    if (/\.tsx?$/.test(p)) {
+      const contents = await fsp.readFile(p, { encoding: "utf-8" });
+      if (!contents.startsWith("/**")) {
+        await fsp.writeFile(p, header + contents);
+      }
+    }
+  }
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(2);
+});
--- src/common/SplitView/index.tsx
+++ src/common/SplitView/index.tsx
@@ -1,3 +1,8 @@
+/**
+ * (c) 2021, Micro:bit Educational Foundation and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
 import SplitView from "./SplitView";
 import SplitViewRemainder from "./SplitViewRemainer";
 import SplitViewDivider from "./SplitViewDivider";
