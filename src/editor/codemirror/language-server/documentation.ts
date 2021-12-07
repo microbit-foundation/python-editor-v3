2eaa06eaec50b041a7311763de03f0b5845c02aa
Matt Hillsdon <matt.hillsdon@microbit.org>
2021-12-07 12:19:23 +0000

    Fix the reference link icon.
    I'd broken the layout at some point by accidentally moving layout code
    inside the click handler. Fix that too.

--- src/editor/codemirror/language-server/documentation.ts
+++ src/editor/codemirror/language-server/documentation.ts
@@ -83,19 +83,27 @@ export const renderMarkdown = (
   };
 };
 
-export const wrapWithDocumentationButton = (child: Element, id: string) => {
-  const wrapper = document.createElement("div");
-  wrapper.style.display = "flex";
-  wrapper.style.height = "100%";
-  wrapper.style.flexDirection = "column";
-  wrapper.style.justifyContent = "space-between";
-  wrapper.appendChild(child);
+export const wrapWithDocumentationButton = (
+  child: Element,
+  id: string
+): Element => {
+  const docsAndActions = document.createElement("div");
+  docsAndActions.style.display = "flex";
+  docsAndActions.style.height = "100%";
+  docsAndActions.style.flexDirection = "column";
+  docsAndActions.style.justifyContent = "space-between";
+  docsAndActions.appendChild(child);
 
-  const button = child.appendChild(document.createElement("button"));
+  const button = docsAndActions.appendChild(document.createElement("button"));
   button.innerHTML =
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M2 3.993A1 1 0 0 1 2.992 3h18.016c.548 0 .992.445.992.993v16.014a1 1 0 0 1-.992.993H2.992A.993.993 0 0 1 2 20.007V3.993zM12 5v14h8V5h-8zm1 2h6v2h-6V7zm0 3h6v2h-6v-2z"/></svg>';
+    '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M5 2.5l.5-.5h2l.5.5v11l-.5.5h-2l-.5-.5v-11zM6 3v10h1V3H6zm3.171.345l.299-.641 1.88-.684.64.299 3.762 10.336-.299.641-1.879.684-.64-.299L9.17 3.345zm1.11.128l3.42 9.396.94-.341-3.42-9.397-.94.342zM1 2.5l.5-.5h2l.5.5v11l-.5.5h-2l-.5-.5v-11zM2 3v10h1V3H2z"></path></svg>';
   // TODO: How do we get translations here?
   button.ariaLabel = "Show reference documentation";
+  button.style.display = "block";
+  button.style.margin = "0";
+  button.style.marginRight = "-0.5rem";
+  button.style.padding = "0.5rem";
+  button.style.alignSelf = "flex-end";
   button.onclick = () => {
     document.dispatchEvent(
       new CustomEvent("cm/openDocs", {
@@ -104,13 +112,6 @@ export const wrapWithDocumentationButton = (child: Element, id: string) => {
         },
       })
     );
-    button.style.display = "block";
-    button.style.margin = "0";
-    button.style.marginRight = "-0.5rem";
-    button.style.padding = "0.5rem";
-    button.style.alignSelf = "flex-end";
-
-    wrapper.appendChild(button);
   };
-  return wrapper;
+  return docsAndActions;
 };
