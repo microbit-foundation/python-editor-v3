86722932fcdc0c97813a3fb9cb2f2250ac3398d2
Matt Hillsdon <44397098+microbit-matt-hillsdon@users.noreply.github.com>
2021-12-17 14:27:00 +0000

    Add a flag to disable the welcome dialog. (#375)
    * Add a flag to disable the welcome dialog.
    
    We plan to use this for user testing, particularly with the French
    translation as we won't be translating this dialog.
    
    * Make it easy to test a single flag in review/local

---
+++ src/workbench/flags.test.ts
@@ -0,0 +1,45 @@
+import { flagsForParams } from "../flags";
+
+describe("flags", () => {
+  it("enables everything in REVIEW", () => {
+    const params = new URLSearchParams([]);
+
+    const flags = flagsForParams("REVIEW", params);
+
+    expect(Object.values(flags).every((x) => x)).toEqual(true);
+  });
+
+  for (const stage of ["STAGING", "PRODUCTION"]) {
+    it("enables nothing in " + stage, () => {
+      const params = new URLSearchParams([]);
+
+      const flags = flagsForParams(stage, params);
+
+      expect(Object.values(flags).every((x) => x)).toEqual(false);
+    });
+  }
+
+  it("enable specific flag", () => {
+    const params = new URLSearchParams([["flag", "noWelcome"]]);
+
+    const flags = flagsForParams("PRODUCTION", params);
+
+    expect(
+      Object.entries(flags).every(
+        ([flag, status]) => (flag === "noWelcome") === status
+      )
+    ).toEqual(true);
+  });
+
+  it("can combine none with specific enabled flags in REVIEW", () => {
+    const params = new URLSearchParams([
+      ["flag", "none"],
+      ["flag", "noWelcome"],
+    ]);
+
+    const flags = flagsForParams("REVIEW", params);
+
+    expect(flags.dnd).toBe(false);
+    expect(flags.noWelcome).toBe(true);
+  });
+});
