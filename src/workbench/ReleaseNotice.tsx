a50fa3c5626fbce48786aa7d6a7d5b658ef69577
Matt Hillsdon <44397098+microbit-matt-hillsdon@users.noreply.github.com>
2021-12-19 12:38:36 +0000

    Add missing licence headers. (#381)

--- src/workbench/ReleaseNotice.tsx
+++ src/workbench/ReleaseNotice.tsx
@@ -1,3 +1,8 @@
+/**
+ * (c) 2021, Micro:bit Educational Foundation and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
 import { Button } from "@chakra-ui/button";
 import { Flex, HStack, Text } from "@chakra-ui/layout";
 import { useCallback, useEffect, useState } from "react";
