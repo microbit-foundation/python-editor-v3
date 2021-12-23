f4f4999490af503c0eec9482fca5ccf31a2bfd1d
Robert Knight <95928279+microbit-robert@users.noreply.github.com>
2021-12-23 10:36:35 +0000

    Reload toolkit on language change (#395)
    There are no translations yet but we'll be adding French in new year.
    
    Closes #390

--- src/documentation/ToolkitDocumentation/api.ts
+++ src/documentation/ToolkitDocumentation/api.ts
@@ -7,8 +7,12 @@ import { Toolkit } from "./model";
 
 // For now we just slurp the whole toolkit at once.
 // Might revisit depending on eventual size.
-const toolkitQuery = `
-  *[_type == "toolkit" && slug.current == "explore" && !(_id in path("drafts.**"))]{
+const toolkitQuery = (languageId: string): string => {
+  if (!languageId.match(/^[a-z-]+$/g)) {
+    throw new Error("Invalid language id.");
+  }
+  return `
+  *[_type == "toolkit" && language == "${languageId}" && slug.current == "explore" && !(_id in path("drafts.**"))]{
     id, name, description,
     contents[]->{
       name, compatibility, subtitle, introduction,
@@ -17,32 +21,44 @@ const toolkitQuery = `
       }
     }
   }`;
+};
 // No need to add a Sanity client dependency just for this.
-const toolkitQueryUrl =
-  "https://ajwvhvgo.api.sanity.io/v1/data/query/apps?query=" +
-  encodeURIComponent(toolkitQuery);
+const toolkitQueryUrl = (languageId: string): string => {
+  return (
+    "https://ajwvhvgo.api.sanity.io/v1/data/query/apps?query=" +
+    encodeURIComponent(toolkitQuery(languageId))
+  );
+};
 
-const fetchToolkitInternal = async (): Promise<Toolkit> => {
-  const response = await fetch(toolkitQueryUrl);
+const fetchToolkitInternal = async (
+  languageId: string
+): Promise<Toolkit | undefined> => {
+  const response = await fetch(toolkitQueryUrl(languageId));
   if (response.ok) {
     const { result } = await response.json();
     if (!result) {
       throw new Error("Unexpected response format");
     }
     const toolkits = result as Toolkit[];
-    if (toolkits.length !== 1) {
-      throw new Error("Could not find expected toolkit");
+    if (toolkits.length === 0) {
+      return undefined;
+    }
+    if (toolkits.length > 1) {
+      throw new Error("Unexpected results");
     }
     return toolkits[0];
   }
   throw new Error("Error fetching toolkit content: " + response.status);
 };
 
-let promise: Promise<Toolkit> | undefined;
-
-export const fetchToolkit = async (): Promise<Toolkit> => {
-  if (!promise) {
-    promise = fetchToolkitInternal();
+export const fetchToolkit = async (languageId: string): Promise<Toolkit> => {
+  const preferred = await fetchToolkitInternal(languageId);
+  if (preferred) {
+    return preferred;
   }
-  return await promise;
+  const fallback = await fetchToolkitInternal("en");
+  if (!fallback) {
+    throw new Error("English toolkit must exist");
+  }
+  return fallback;
 };
