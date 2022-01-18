f150202c8d72a087765b739746dccdd20fcd9bc8
Robert Knight <robert@microbit.org>
2022-01-18 10:08:36 +0000

    Index reference docs for search

--- src/documentation/SearchProvider.tsx
+++ src/documentation/SearchProvider.tsx
@@ -11,6 +11,11 @@ interface Search {
   search(text: string): string[];
 }
 
+interface searchableContent {
+  id: string;
+  text: string | undefined;
+}
+
 const SearchContext = createContext<Search | undefined>(undefined);
 
 export const useSearch = (): Search => {
@@ -22,42 +27,37 @@ export const useSearch = (): Search => {
 };
 
 const SearchProvider = ({ children }: { children: ReactNode }) => {
-  const { exploreToolkit, referenceToolkit } = useToolkitState();
+  const { referenceToolkit } = useToolkitState();
 
   const value: Search = useMemo(() => {
-    // Use toolkits here to create index rather than hardcoded example.
-    const documents = [
-      {
-        id: "Lunr",
-        text: "Like Solr, but much smaller, and not as bright.",
-      },
-      {
-        id: "React",
-        text: "A JavaScript library for building user interfaces.",
-      },
-      {
-        id: "Lodash",
-        text: "A modern JavaScript utility library delivering modularity, performance & extras and very modern.",
-      },
-    ];
+    const searchableReferenceContent: searchableContent[] = [];
 
-    const index = lunr(function () {
+    if (referenceToolkit) {
+      for (const doc in referenceToolkit) {
+        searchableReferenceContent.push({
+          id: referenceToolkit[doc].id,
+          text: referenceToolkit[doc].docString,
+        });
+      }
+    }
+
+    const refIndex = lunr(function () {
       this.ref("id");
       this.field("text");
       this.metadataWhitelist = ["position"];
-      for (const doc of documents) {
+      for (const doc of searchableReferenceContent) {
         this.add(doc);
       }
     });
 
     return {
       search: (text: string) => {
-        const results = index.search(text);
+        const results = refIndex.search(text);
         console.log(results);
         return results.map((r) => r.ref);
       },
     };
-  }, [exploreToolkit, referenceToolkit]);
+  }, [referenceToolkit]);
 
   return (
     <SearchContext.Provider value={value}>{children}</SearchContext.Provider>
