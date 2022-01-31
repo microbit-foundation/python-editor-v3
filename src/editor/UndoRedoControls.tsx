236b1c3718dda721e95b09c498a95117f70b97a8
Robert Knight <robert@microbit.org>
2022-01-31 15:10:07 +0000

    Add logging for simple events

--- src/editor/UndoRedoControls.tsx
+++ src/editor/UndoRedoControls.tsx
@@ -1,20 +1,22 @@
 /**
- * (c) 2021, Micro:bit Educational Foundation and contributors
+ * (c) 2022, Micro:bit Educational Foundation and contributors
  *
  * SPDX-License-Identifier: MIT
  */
 import { ButtonGroup, IconButton } from "@chakra-ui/react";
-import { RiArrowGoForwardLine, RiArrowGoBackLine } from "react-icons/ri";
+import { RiArrowGoBackLine, RiArrowGoForwardLine } from "react-icons/ri";
 import { useIntl } from "react-intl";
 import {
   useActiveEditorActions,
   useActiveEditorInfo,
 } from "../editor/active-editor-hooks";
+import { useLogging } from "../logging/logging-hooks";
 
 const UndoRedoControls = ({ ...props }) => {
   const intl = useIntl();
   const actions = useActiveEditorActions();
   const editorInfo = useActiveEditorInfo();
+  const logging = useLogging();
 
   return (
     <ButtonGroup
@@ -29,7 +31,10 @@ const UndoRedoControls = ({ ...props }) => {
         isRound
         icon={<RiArrowGoBackLine style={{ transform: "rotate(-90deg)" }} />}
         aria-label={intl.formatMessage({ id: "undo" })}
-        onClick={actions?.undo}
+        onClick={() => {
+          actions?.undo();
+          logging.event({ type: "undo" });
+        }}
         disabled={editorInfo.undo ? false : true}
       />
       <IconButton
@@ -38,7 +43,10 @@ const UndoRedoControls = ({ ...props }) => {
         borderLeftColor="gray.10"
         icon={<RiArrowGoForwardLine style={{ transform: "rotate(-90deg)" }} />}
         aria-label={intl.formatMessage({ id: "redo" })}
-        onClick={actions?.redo}
+        onClick={() => {
+          actions?.redo();
+          logging.event({ type: "redo" });
+        }}
         disabled={editorInfo.redo ? false : true}
       />
     </ButtonGroup>
