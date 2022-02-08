374190e348cdb04430c414b5e3b14301e70aa31c
Robert Knight <robert@microbit.org>
2022-02-08 14:10:11 +0000

    Change theme to absolute import

--- craco.config.js
+++ craco.config.js
@@ -7,10 +7,10 @@ const path = require("path");
 const fs = require("fs");
 
 // Support optionally pulling in external branding if the module is installed.
-const external =
-  "node_modules/@microbit-foundation/python-editor-next-microbit";
+const theme = "@microbit-foundation/python-editor-next-microbit";
+const external = `node_modules/${theme}`;
 const internal = "src/deployment/default";
-const location = fs.existsSync(external) ? external : internal;
+const location = fs.existsSync(external) ? theme : internal;
 
 module.exports = {
   webpack: {
