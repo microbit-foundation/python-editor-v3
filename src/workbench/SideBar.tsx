e0fdca1dbdef53b454dd70f231262564f22cd7e6
Robert Knight <95928279+microbit-robert@users.noreply.github.com>
2022-02-14 14:34:30 +0000

    Multiple sidebar changes (#591)
    Changes Files icon to multiple files.
    Adds Projects placeholder below Explore.
    Aim is to slightly deemphasis reference and see if this helps nudge folks to Explore in user testing.
    
    Closes #584.

--- src/workbench/SideBar.tsx
+++ src/workbench/SideBar.tsx
@@ -1,5 +1,5 @@
 /**
- * (c) 2021, Micro:bit Educational Foundation and contributors
+ * (c) 2022, Micro:bit Educational Foundation and contributors
  *
  * SPDX-License-Identifier: MIT
  */
@@ -16,12 +16,13 @@ import {
 } from "@chakra-ui/react";
 import { ReactNode, useCallback, useMemo } from "react";
 import { IconType } from "react-icons";
-import { RiFolderFill } from "react-icons/ri";
-import { VscLibrary } from "react-icons/vsc";
+import { RiLightbulbFlashLine } from "react-icons/ri";
+import { VscFiles, VscLibrary } from "react-icons/vsc";
 import { useIntl } from "react-intl";
 import ErrorBoundary from "../common/ErrorBoundary";
 import PythonLogo from "../common/PythonLogo";
 import ExploreArea from "../documentation/ExploreArea";
+import ProjectsArea from "../documentation/ProjectsArea";
 import ReferenceArea from "../documentation/ReferenceArea";
 import FilesArea from "../files/FilesArea";
 import FilesAreaNav from "../files/FilesAreaNav";
@@ -71,6 +72,13 @@ const SideBar = ({
         color: "gray.25",
       },
       {
+        id: "projects",
+        title: intl.formatMessage({ id: "projects-tab" }),
+        icon: RiLightbulbFlashLine,
+        contents: <ProjectsArea />,
+        color: "gray.25",
+      },
+      {
         id: "reference",
         title: intl.formatMessage({ id: "reference-tab" }),
         // If you change this icon you also need to change the version embedded
@@ -83,7 +91,7 @@ const SideBar = ({
       {
         id: "files",
         title: intl.formatMessage({ id: "files-tab" }),
-        icon: RiFolderFill,
+        icon: VscFiles,
         nav: <FilesAreaNav />,
         contents: (
           <FilesArea
