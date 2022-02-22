587b1055352186a468b136a603bcd46d6b261c01
Robert Knight <95928279+microbit-robert@users.noreply.github.com>
2022-02-22 12:04:20 +0000

    Menus always appear above hovered code examples (#609)
    Prevents code pop overs from blocking open menus using z-index..
    
    Closes https://github.com/microbit-foundation/python-editor-next/issues/582.

--- src/common/zIndex.ts
+++ src/common/zIndex.ts
@@ -1,7 +1,8 @@
 /**
- * (c) 2021, Micro:bit Educational Foundation and contributors
+ * (c) 2022, Micro:bit Educational Foundation and contributors
  *
  * SPDX-License-Identifier: MIT
  */
+export const zIndexCodePopUp = 2;
 // z-index above the xterm.js's layers (currently 10 but given some margin for increases as it can vary with config)
 export const zIndexAboveTerminal = 20;
