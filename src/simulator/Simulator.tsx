e87cac0fdaccc5f15ad24e2702e5cd65b0e6559b
Matt Hillsdon <matt.hillsdon@microbit.org>
2022-03-18 17:10:01 +0000

    Add the sensor.

--- src/simulator/Simulator.tsx
+++ src/simulator/Simulator.tsx
@@ -1,11 +1,34 @@
-import { Box, HStack, IconButton, Stack } from "@chakra-ui/react";
-import { useCallback, useEffect, useRef } from "react";
-import { RiPlayFill, RiStopFill } from "react-icons/ri";
+import {
+  AspectRatio,
+  Box,
+  BoxProps,
+  HStack,
+  Icon,
+  IconButton,
+  Slider,
+  SliderFilledTrack,
+  SliderMark,
+  SliderThumb,
+  SliderTrack,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { RiPlayFill, RiStopFill, RiSunFill } from "react-icons/ri";
 import { MAIN_FILE } from "../fs/fs";
 import { useFileSystem } from "../fs/fs-hooks";
 
+interface Sensor {
+  type: "range";
+  id: string;
+  min: number;
+  max: number;
+  value: number;
+}
+
 const useSimulator = (ref: React.RefObject<HTMLIFrameElement>) => {
   const fs = useFileSystem();
+  const [sensors, setSensors] = useState<Record<string, Sensor>>({});
   const readyCallbacks = useRef([] as Array<() => void>);
   useEffect(() => {
     if (ref.current) {
@@ -14,7 +37,13 @@ const useSimulator = (ref: React.RefObject<HTMLIFrameElement>) => {
         if (e.source === simulator) {
           switch (e.data.kind) {
             case "ready": {
-              // TODO: sensors
+              setSensors(
+                Object.fromEntries(
+                  e.data.sensors.map((json: Sensor) => {
+                    return [json.id, json];
+                  })
+                )
+              );
               while (readyCallbacks.current.length) {
                 readyCallbacks.current.pop()!();
               }
@@ -33,6 +62,25 @@ const useSimulator = (ref: React.RefObject<HTMLIFrameElement>) => {
       };
     }
   }, [ref, readyCallbacks]);
+
+  const onSensorChange = useCallback(
+    (id: string, value: number) => {
+      setSensors((sensors) => ({
+        ...sensors,
+        [id]: { ...sensors[id], value },
+      }));
+      const simulator = ref.current!.contentWindow!;
+      simulator.postMessage(
+        {
+          kind: "sensor_set",
+          sensor: id,
+          value,
+        },
+        "*"
+      );
+    },
+    [ref, setSensors]
+  );
 
   const play = useCallback(async () => {
     // Temporary approach until we have simulator filesystem support.
@@ -75,30 +123,127 @@ const useSimulator = (ref: React.RefObject<HTMLIFrameElement>) => {
   return {
     play,
     stop,
+    sensors: Object.values(sensors),
+    onSensorChange,
   };
 };
 
 const Simulator = () => {
   const ref = useRef<HTMLIFrameElement>(null);
-  const { play, stop } = useSimulator(ref);
+  const { play, stop, sensors, onSensorChange } = useSimulator(ref);
   return (
-    <Stack>
-      <Box
-        ref={ref}
-        as="iframe"
-        // Very much a temporary / unsupported deployment!
-        src="https://distracted-dubinsky-fd8a42.netlify.app/simulator.html"
-        title="Simulator"
-        frameBorder="no"
-        scrolling="no"
-        width={300}
-        height={241}
+    <VStack spacing={5}>
+      <Box width="100%">
+        <AspectRatio ratio={400 / 321} width="100%">
+          <Box
+            ref={ref}
+            as="iframe"
+            // Very much a temporary / unsupported deployment!
+            src="https://distracted-dubinsky-fd8a42.netlify.app/simulator.html"
+            title="Simulator"
+            frameBorder="no"
+            scrolling="no"
+          />
+        </AspectRatio>
+        {/* Margin hack until we remove space from iframe */}
+        <HStack justifyContent="center" mt={-2}>
+          <IconButton
+            variant="solid"
+            onClick={play}
+            icon={<RiPlayFill />}
+            aria-label="Run"
+          />
+          <IconButton
+            variant="outline"
+            onClick={stop}
+            icon={<RiStopFill />}
+            aria-label="Stop"
+          />
+        </HStack>
+      </Box>
+      <Sensors
+        value={sensors}
+        flex="1 1 auto"
+        onSensorChange={onSensorChange}
       />
-      <HStack justifyContent="center">
-        <IconButton onClick={play} icon={<RiPlayFill />} aria-label="Play" />
-        <IconButton onClick={stop} icon={<RiStopFill />} aria-label="Stop" />
-      </HStack>
+    </VStack>
+  );
+};
+
+interface SensorsProps extends BoxProps {
+  value: Sensor[];
+  onSensorChange: (id: string, value: number) => void;
+}
+
+const Sensors = ({ value, onSensorChange, ...props }: SensorsProps) => {
+  return (
+    <Stack {...props} height="100%" width="100%" p={5}>
+      {value.map((sensor) => {
+        switch (sensor.type) {
+          case "range":
+            return (
+              <RangeSensor
+                key={sensor.id}
+                value={sensor}
+                onSensorChange={onSensorChange}
+              />
+            );
+          default:
+            return null;
+        }
+      })}
     </Stack>
+  );
+};
+
+interface RangeSensorProps {
+  value: Sensor;
+  onSensorChange: (id: string, value: number) => void;
+}
+
+const RangeSensor = ({
+  value: { id, min, max, value },
+  onSensorChange,
+}: RangeSensorProps) => {
+  const handleChange = useCallback(
+    (value: number) => {
+      onSensorChange(id, value);
+    },
+    [onSensorChange, id]
+  );
+  return (
+    <HStack>
+      <Icon as={RiSunFill} aria-label={id} color="blimpTeal.400" boxSize="6" />
+      <Slider
+        aria-label={id}
+        value={value}
+        min={min}
+        max={max}
+        onChange={handleChange}
+        my={5}
+      >
+        <SliderTrack>
+          <SliderFilledTrack bgColor="blimpTeal.600" />
+        </SliderTrack>
+        <SliderThumb />
+        <SliderMark value={min} mt="1" fontSize="xs">
+          {min}
+        </SliderMark>
+        <SliderMark value={max} mt="1" ml="-3ch" fontSize="xs">
+          {max}
+        </SliderMark>
+        <SliderMark
+          value={value}
+          textAlign="center"
+          mt="-8"
+          ml="-1.5ch"
+          w="3ch"
+          fontSize="sm"
+        >
+          {value}
+        </SliderMark>
+      </Slider>
+    </HStack>
   );
 };
 
