a833c03b498a88eac40639a4e3b1b4b46a1097fc
Matt Hillsdon <matt.hillsdon@microbit.org>
2022-04-12 15:54:38 +0100

    Extract common code for querying Sanity CMS.

--- src/documentation/ideas/ideas-api.ts
+++ src/documentation/ideas/ideas-api.ts
@@ -3,65 +3,36 @@
  *
  * SPDX-License-Identifier: MIT
  */
+import { fetchContent } from "../../common/sanity";
 import { Idea } from "./model";
 
-// Temporarily fetch drafts
-const toolkitQuery = (languageId: string): string => {
+export const fetchIdeasToolkit = async (languageId: string): Promise<Idea[]> =>
+  fetchContent(languageId, ideasQuery, adaptContent);
+
+const ideasQuery = (languageId: string): string => {
   if (!languageId.match(/^[a-z-]+$/g)) {
     throw new Error("Invalid language id.");
   }
   return `
-  *[_type == "pythonIdea" && language == "${languageId}" && !(_id in path("drafts.**"))]{
-    _id, name, language, compatibility, image, slug,
-    content[] {
-      ...,
-      markDefs[]{
+    *[_type == "pythonIdea" && language == "${languageId}" && !(_id in path("drafts.**"))]{
+      _id, name, language, compatibility, image, slug,
+      content[] {
         ...,
-        _type == "toolkitInternalLink" => {
-            "slug": @.reference->slug,
-            "targetType": @.reference->_type
+        markDefs[]{
+          ...,
+          _type == "toolkitInternalLink" => {
+              "slug": @.reference->slug,
+              "targetType": @.reference->_type
+          }
         }
-      }
-    },
-  }`;
+      },
+    }`;
 };
 
-// No need to add a Sanity client dependency just for this.
-const toolkitQueryUrl = (languageId: string): string => {
-  return (
-    "https://ajwvhvgo.api.sanity.io/v1/data/query/apps?query=" +
-    encodeURIComponent(toolkitQuery(languageId))
-  );
-};
-
-const fetchToolkitInternal = async (
-  languageId: string
-): Promise<Idea[] | undefined> => {
-  const response = await fetch(toolkitQueryUrl(languageId));
-  if (response.ok) {
-    const { result } = await response.json();
-    if (!result) {
-      throw new Error("Unexpected response format");
-    }
-    const toolkit = result as Idea[];
-    if (toolkit.length === 0) {
-      return undefined;
-    }
-    return toolkit;
+const adaptContent = (result: any): Idea[] | undefined => {
+  const toolkit = result as Idea[];
+  if (toolkit.length === 0) {
+    return undefined;
   }
-  throw new Error("Error fetching toolkit content: " + response.status);
-};
-
-export const fetchIdeasToolkit = async (
-  languageId: string
-): Promise<Idea[]> => {
-  const preferred = await fetchToolkitInternal(languageId);
-  if (preferred) {
-    return preferred;
-  }
-  const fallback = await fetchToolkitInternal("en");
-  if (!fallback) {
-    throw new Error("English toolkit must exist");
-  }
-  return [];
+  return toolkit;
 };
--- src/documentation/reference/api.ts
+++ src/documentation/reference/api.ts
@@ -3,7 +3,30 @@
  *
  * SPDX-License-Identifier: MIT
  */
+import { fetchContent } from "../../common/sanity";
 import { Toolkit, ToolkitTopic, ToolkitTopicEntry } from "./model";
+
+export const fetchToolkit = async (languageId: string): Promise<Toolkit> =>
+  fetchContent(languageId, toolkitQuery, adaptContent);
+
+export const getTopicAndEntry = (
+  toolkit: Toolkit,
+  topicOrEntryId: string | undefined
+): [ToolkitTopic | undefined, ToolkitTopicEntry | undefined] => {
+  const topic = toolkit.contents?.find(
+    (t) => t.slug.current === topicOrEntryId
+  );
+  if (topic) {
+    return [topic, undefined];
+  }
+  const entry = toolkit.contents
+    ?.flatMap((topic) => topic.contents ?? [])
+    .find((entry) => entry.slug.current === topicOrEntryId);
+  if (!entry) {
+    return [undefined, undefined];
+  }
+  return [entry.parent, entry];
+};
 
 // For now we just slurp the whole toolkit at once.
 // Might revisit depending on eventual size.
@@ -53,69 +76,21 @@ const toolkitQuery = (languageId: string): string => {
     }
   }`;
 };
-// No need to add a Sanity client dependency just for this.
-const toolkitQueryUrl = (languageId: string): string => {
-  return (
-    "https://ajwvhvgo.api.sanity.io/v1/data/query/apps?query=" +
-    encodeURIComponent(toolkitQuery(languageId))
-  );
-};
 
-const fetchToolkitInternal = async (
-  languageId: string
-): Promise<Toolkit | undefined> => {
-  const response = await fetch(toolkitQueryUrl(languageId));
-  if (response.ok) {
-    const { result } = await response.json();
-    if (!result) {
-      throw new Error("Unexpected response format");
-    }
-    const toolkits = result as Toolkit[];
-    if (toolkits.length === 0) {
-      return undefined;
-    }
-    if (toolkits.length > 1) {
-      throw new Error("Unexpected results");
-    }
-    // Add topic entry parent for toolkit navigation.
-    const toolkit = toolkits[0];
-    toolkit.contents?.forEach((topic) => {
-      topic.contents?.forEach((entry) => {
-        entry.parent = topic;
-      });
+const adaptContent = (result: any): Toolkit | undefined => {
+  const toolkits = result as Toolkit[];
+  if (toolkits.length === 0) {
+    return undefined;
+  }
+  if (toolkits.length > 1) {
+    throw new Error("Unexpected results");
+  }
+  // Add topic entry parent for toolkit navigation.
+  const toolkit = toolkits[0];
+  toolkit.contents?.forEach((topic) => {
+    topic.contents?.forEach((entry) => {
+      entry.parent = topic;
     });
-    return toolkit;
-  }
-  throw new Error("Error fetching toolkit content: " + response.status);
-};
-
-export const fetchToolkit = async (languageId: string): Promise<Toolkit> => {
-  const preferred = await fetchToolkitInternal(languageId);
-  if (preferred) {
-    return preferred;
-  }
-  const fallback = await fetchToolkitInternal("en");
-  if (!fallback) {
-    throw new Error("English toolkit must exist");
-  }
-  return fallback;
-};
-
-export const getTopicAndEntry = (
-  toolkit: Toolkit,
-  topicOrEntryId: string | undefined
-): [ToolkitTopic | undefined, ToolkitTopicEntry | undefined] => {
-  const topic = toolkit.contents?.find(
-    (t) => t.slug.current === topicOrEntryId
-  );
-  if (topic) {
-    return [topic, undefined];
-  }
-  const entry = toolkit.contents
-    ?.flatMap((topic) => topic.contents ?? [])
-    .find((entry) => entry.slug.current === topicOrEntryId);
-  if (!entry) {
-    return [undefined, undefined];
-  }
-  return [entry.parent, entry];
+  });
+  return toolkit;
 };
