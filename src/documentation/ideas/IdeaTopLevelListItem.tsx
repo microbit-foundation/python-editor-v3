b91a0387c6e051b8da4c8b342071523630fc918c
Matt Hillsdon <matt.hillsdon@microbit.org>
2022-04-12 10:08:49 +0100

    Card tweaks.

--- src/documentation/ideas/IdeaTopLevelListItem.tsx
+++ src/documentation/ideas/IdeaTopLevelListItem.tsx
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: MIT
  */
 import { VStack } from "@chakra-ui/layout";
-import { Box, HStack, Image } from "@chakra-ui/react";
+import { Box, HStack, Image, Text } from "@chakra-ui/react";
 import { imageUrlBuilder } from "../../common/imageUrlBuilder";
 import ToolkitName from "../reference/ToolkitName";
 import { IdeaImage } from "./model";
@@ -26,33 +26,27 @@ const IdeaTopLevelListItem = ({
     <Box onClick={onForward} cursor="pointer">
       <HStack
         spacing={5}
-        background={"white"}
-        padding={1}
-        paddingBottom={2}
-        paddingTop={1.5}
-        borderRadius={"lg"}
+        background="white"
+        borderRadius="lg"
         overflow="hidden"
-        boxShadow="4px 0px 24px #00000033"
+        boxShadow="md"
         // Fill available height to keep grid items the same size.
-        height={"100%"}
-        alignItems={"flex-start"}
+        height="100%"
+        alignItems="flex-start"
       >
         <VStack alignItems="center" spacing={2} flex="1 1 auto">
           <Image
             borderTopRadius={"lg"}
             src={imageUrlBuilder.image(image.asset).url()}
             alt=""
-            // Something odd happens with the background color
-            // to right of the image at widths above this.
-            width="98%"
           />
           <ToolkitName
             alignSelf="flex-start"
-            paddingLeft={2}
-            paddingRight={2}
+            px={2.5}
+            pb={2}
             name={name}
             isV2Only={!!isV2Only}
-          ></ToolkitName>
+          />
         </VStack>
       </HStack>
     </Box>
