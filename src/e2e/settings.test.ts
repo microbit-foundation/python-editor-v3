2d294f24f485ec18cf5ba7156cec1f0fc58a3cf9
Matt Hillsdon <matt.hillsdon@microbit.org>
2022-04-22 14:31:50 +0100

    e2e test.

--- src/e2e/settings.test.ts
+++ src/e2e/settings.test.ts
@@ -7,9 +7,26 @@ import { App } from "./app";
 
 describe("Browser - settings", () => {
   const app = new App();
-  beforeEach(app.reset.bind(app));
+  beforeEach(() => {
+    app.setOptions({});
+    return app.reset();
+  });
   afterEach(app.screenshot.bind(app));
   afterAll(app.dispose.bind(app));
+
+  it("sets language via URL", async () => {
+    app.setOptions({
+      language: "fr",
+    });
+    await app.reset();
+    // French via the URL
+    await app.findProjectName("Projet sans titre");
+
+    await app.switchLanguage("en");
+    await app.reset();
+    // French URL ignored as we've made an explicit language choice.
+    await app.findProjectName("Untitled project");
+  });
 
   it("switches language", async () => {
     // NOTE: the app methods generally won't still work after changing language.
