424ff5ac9096ae54b64e9cbffeeb4e3e5e905a21
Robert Knight <robert@microbit.org>
2022-04-25 10:22:20 +0100

    Tweak color, remove underline, add arrow icon

--- src/documentation/common/ShowMoreButton.tsx
+++ src/documentation/common/ShowMoreButton.tsx
@@ -5,14 +5,24 @@
  */
 import { Link, LinkProps } from "@chakra-ui/react";
 import { FormattedMessage } from "react-intl";
+import ExpandCollapseIcon from "../../common/ExpandCollapseIcon";
 
 interface ShowMoreLinkProps extends LinkProps {
   isOpen: boolean;
 }
 
 const ShowMoreButton = ({ isOpen, ...props }: ShowMoreLinkProps) => (
-  <Link {...props} as="button" color="brand.600" textAlign="left">
+  <Link
+    {...props}
+    as="button"
+    color="brand.500"
+    textAlign="left"
+    _hover={{
+      textDecoration: "none",
+    }}
+  >
     <FormattedMessage id={isOpen ? "show-less" : "show-more"} />
+    <ExpandCollapseIcon open={isOpen} ml={1} />
   </Link>
 );
 
