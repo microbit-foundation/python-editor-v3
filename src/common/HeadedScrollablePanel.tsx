1ede40cf01c6c3c665381d33d6d036acfeda1cde
Robert Knight <robert@microbit.org>
2022-05-04 16:26:24 +0100

    Prevent code embed popup through breadcrumb

--- src/common/HeadedScrollablePanel.tsx
+++ src/common/HeadedScrollablePanel.tsx
@@ -23,7 +23,7 @@ const HeadedScrollablePanel = ({
   <ScrollablePanel>
     <Slide direction={direction}>
       <VStack alignItems="stretch" spacing={0} {...props}>
-        <Box bg="gray.25" flex="0 0 auto" position="sticky" top="0" zIndex={1}>
+        <Box bg="gray.25" flex="0 0 auto" position="sticky" top="0" zIndex={2}>
           {/* Use of header here is relied on to correctly offset the scrolling based on the actual height of the sticky header. See Highlight.tsx */}
           <Box as="header" p={5} pt={3}>
             {heading}
