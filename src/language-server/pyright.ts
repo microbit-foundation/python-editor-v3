1ab3fa5c5224c1402c8fc64314e05a8b0c727822
Robert Knight <robert@microbit.org>
2022-06-09 14:34:12 +0100

    Update Pyright.

--- src/language-server/pyright.ts
+++ src/language-server/pyright.ts
@@ -11,7 +11,7 @@ import {
 import { createUri, LanguageServerClient } from "./client";
 
 // This is modified by bin/update-pyright.sh
-const workerScriptName = "pyright-main-4c1fe7a1b366bfb47dd7.worker.js";
+const workerScriptName = "pyright-main-8fe8afc1823da211c82b.worker.js";
 
 /**
  * Creates Pyright workers and corresponding client.
