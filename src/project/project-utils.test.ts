7bf5bb3ca0c8263308a7879151acb1ae6d139409
Matt Hillsdon <44397098+microbit-matt-hillsdon@users.noreply.github.com>
2022-06-15 10:34:17 +0100

    Fix module name validation. (#770)
    We haven't checked the exact unicode ranges as that's quite involved but
    this should generally be more accurate and no longer ban numbers or
    characters from languages without an upper/lowercase distinction.
    
    Closes https://github.com/microbit-foundation/python-editor-next/issues/524
    Closes https://github.com/microbit-foundation/python-editor-next/issues/531

--- src/project/project-utils.test.ts
+++ src/project/project-utils.test.ts
@@ -2,6 +2,8 @@
  * (c) 2021, Micro:bit Educational Foundation and contributors
  *
  * SPDX-License-Identifier: MIT
+ *
+ * @jest-environment node
  */
 import { validateNewFilename } from "./project-utils";
 import { stubIntl as intl } from "../messages/testing";
@@ -9,26 +11,44 @@ import { stubIntl as intl } from "../messages/testing";
 describe("validateNewFilename", () => {
   const exists = (filename: string) => filename === "main.py";
 
-  it("required non-empty name", () => {
-    expect(validateNewFilename("", exists, intl)).toEqual("name-not-empty");
-  });
-  it("errors for Python extensions", () => {
-    expect(validateNewFilename("foo.py", exists, intl)).toEqual(
-      "lowercase-no-space"
+  it("requires non-empty name", () => {
+    expect(validateNewFilename("", exists, intl)).toEqual(
+      "file-name-not-empty"
     );
+  });
+  it("length", () => {
+    expect(validateNewFilename("a".repeat(121), exists, intl)).toEqual(
+      "file-name-length"
+    );
+    expect(validateNewFilename("a".repeat(120), exists, intl)).toBeUndefined();
+  });
+  it("no error for Python extensions", () => {
+    expect(validateNewFilename("foo.py", exists, intl)).toBeUndefined();
   });
   it("errors for spaces", () => {
     expect(validateNewFilename("spaces are not allowed", exists, intl)).toEqual(
-      "lowercase-no-space"
+      "file-name-whitespace"
+    );
+  });
+  it("errors for other invalid chars", () => {
+    expect(validateNewFilename("wow!64", exists, intl)).toEqual(
+      "file-name-invalid-character"
+    );
+  });
+  it("errors for leading number", () => {
+    expect(validateNewFilename("99greenbottles", exists, intl)).toEqual(
+      "file-name-start-number"
     );
   });
   it("errors for uppercase", () => {
     expect(validateNewFilename("OHNO", exists, intl)).toEqual(
-      "lowercase-no-space"
+      "file-name-lowercase-only"
     );
   });
   it("errors for file clashes", () => {
-    expect(validateNewFilename("main", exists, intl)).toEqual("already-exists");
+    expect(validateNewFilename("main", exists, intl)).toEqual(
+      "file-already-exists"
+    );
   });
   it("accepts valid names", () => {
     expect(
