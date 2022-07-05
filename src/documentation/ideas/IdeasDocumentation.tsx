6ba001dd45db05a0ddaaab86245a1d339b50d3bb
Matt Hillsdon <matt.hillsdon@microbit.org>
2022-07-05 16:36:18 +0100

    More ideas link.
    Demo for discussion with Giles and others.

--- src/documentation/ideas/IdeasDocumentation.tsx
+++ src/documentation/ideas/IdeasDocumentation.tsx
@@ -3,10 +3,10 @@
  *
  * SPDX-License-Identifier: MIT
  */
-import { Stack } from "@chakra-ui/layout";
+import { Link, Stack, Text } from "@chakra-ui/layout";
 import { Image, SimpleGrid } from "@chakra-ui/react";
-import { useCallback, useRef } from "react";
-import { useIntl } from "react-intl";
+import { ReactNode, useCallback, useRef } from "react";
+import { FormattedMessage, useIntl } from "react-intl";
 import AreaHeading from "../../common/AreaHeading";
 import { docStyles } from "../../common/documentation-styles";
 import HeadedScrollablePanel from "../../common/HeadedScrollablePanel";
@@ -143,6 +143,23 @@ const ActiveLevel = ({
           />
         ))}
       </SimpleGrid>
+      <Text pb={8} px={5}>
+        <FormattedMessage
+          id="more-ideas"
+          values={{
+            link: (chunks: ReactNode) => (
+              <Link
+                color="brand.500"
+                href="https://microbit.org/projects/make-it-code-it/?filters=python"
+                target="_blank"
+                rel="noopener"
+              >
+                {chunks}
+              </Link>
+            ),
+          }}
+        />
+      </Text>
     </HeadedScrollablePanel>
   );
 };
