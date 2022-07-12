0f0e1f08d7c52d331203db1ac08f50999fc8a61f
Matt Hillsdon <matt.hillsdon@microbit.org>
2022-07-12 13:09:50 +0100

    Fix paths in test.

--- src/fs/fs.test.ts
+++ src/fs/fs.test.ts
@@ -24,10 +24,10 @@ import { defaultInitialProject } from "./initial-project";
 import { MicroPythonSource } from "../micropython/micropython";
 
 const hexes = Promise.all([
-  fs.readFileSync("src/fs/microbit-micropython-v1.hex", {
+  fs.readFileSync("src/micropython/microbit-micropython-v1.hex", {
     encoding: "ascii",
   }),
-  fs.readFileSync("src/fs/microbit-micropython-v2.hex", {
+  fs.readFileSync("src/micropython/main/microbit-micropython-v2.hex", {
     encoding: "ascii",
   }),
 ]);
