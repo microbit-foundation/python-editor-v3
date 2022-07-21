bcc8cf42c9ac863a24186d20946365cd35af1824
Robert Knight <robert@microbit.org>
2022-07-21 15:36:47 +0100

    Update query to get ideas in config order.

--- src/documentation/ideas/content.ts
+++ src/documentation/ideas/content.ts
@@ -14,25 +14,29 @@ const ideasQuery = (languageId: string): string => {
     throw new Error("Invalid language id.");
   }
   return `
-    *[_type == "pythonIdea" && language == "${languageId}" && !(_id in path("drafts.**"))]{
-      _id, name, language, compatibility, image, slug,
-      content[] {
-        ...,
-        markDefs[]{
+    *[_type == "pythonIdeasConfig" && language == "${languageId}" && !(_id in path("drafts.**"))][0]{
+      pythonIdeasOrder[]->{
+        _id, name, language, compatibility, image, slug,
+        content[] {
           ...,
-          _type == "toolkitInternalLink" => {
-              "slug": @.reference->slug,
-              "targetType": @.reference->_type
+          markDefs[]{
+            ...,
+            _type == "toolkitInternalLink" => {
+                "slug": @.reference->slug,
+                "targetType": @.reference->_type
+            }
           }
-        }
-      },
+        },
+      }
     }`;
 };
 
 const adaptContent = (result: any): Idea[] | undefined => {
-  const ideas = result as Idea[];
-  if (ideas.length === 0) {
-    return undefined;
+  const ideas = result?.pythonIdeasOrder as Idea[];
+  if (ideas) {
+    if (ideas.length === 0) {
+      return undefined;
+    }
   }
   return ideas;
 };
