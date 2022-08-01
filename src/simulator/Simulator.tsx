bde5d30531dbb696e5e44c234dbe731963fe6c87
Robert Knight <robert@microbit.org>
2022-08-01 13:03:55 +0100

    Correct loading state for simulator serial.

--- src/simulator/Simulator.tsx
+++ src/simulator/Simulator.tsx
@@ -1,24 +1,19 @@
+/**
+ * (c) 2022, Micro:bit Educational Foundation and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
 import { AspectRatio, Box, Flex, VStack } from "@chakra-ui/react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useIntl } from "react-intl";
-import {
-  SplitView,
-  SplitViewDivider,
-  SplitViewRemainder,
-  SplitViewSized,
-} from "../common/SplitView";
-import { SizedMode } from "../common/SplitView/SplitView";
 import { DeviceContextProvider } from "../device/device-hooks";
 import { SimulatorDeviceConnection } from "../device/simulator";
-import SerialArea from "../serial/SerialArea";
-import Sensors from "./Sensors";
 import SimulatorActionBar from "./SimulatorActionBar";
+import SimulatorSplitView from "./SimulatorSplitView";
 
 const Simulator = () => {
   const ref = useRef<HTMLIFrameElement>(null);
   const intl = useIntl();
-  const [serialStateWhenOpen, setSerialStateWhenOpen] =
-    useState<SizedMode>("compact");
   const simulator = useRef(
     new SimulatorDeviceConnection(() => {
       return ref.current;
@@ -57,32 +52,7 @@ const Simulator = () => {
             />
           </Box>
         </VStack>
-        <SplitView
-          direction="column"
-          minimums={[150, 200]}
-          compactSize={SerialArea.compactSize}
-          height={`calc(100% - ${simHeight}px)`}
-          mode={serialStateWhenOpen}
-        >
-          <SplitViewSized>
-            <SerialArea
-              as="section"
-              compact={serialStateWhenOpen === "compact"}
-              onSizeChange={setSerialStateWhenOpen}
-              aria-label={intl.formatMessage({
-                id: "serial-terminal",
-              })}
-            />
-          </SplitViewSized>
-          <SplitViewDivider />
-          <SplitViewRemainder overflowY="auto">
-            <Flex flexDirection="column" height="100%">
-              <VStack spacing={5} bg="gray.25" flex="1 1 auto">
-                <Sensors flex="1 1 auto" />
-              </VStack>
-            </Flex>
-          </SplitViewRemainder>
-        </SplitView>
+        <SimulatorSplitView simHeight={simHeight} />
       </Flex>
     </DeviceContextProvider>
   );
