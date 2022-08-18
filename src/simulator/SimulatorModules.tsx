b03681b6745d3652446344c2cea8f5d05a5a85b0
Robert Knight <95928279+microbit-robert@users.noreply.github.com>
2022-08-18 15:26:56 +0100

    Tweak padding and bold sensor titles. (#894)

--- src/simulator/SimulatorModules.tsx
+++ src/simulator/SimulatorModules.tsx
@@ -114,7 +114,8 @@ const SimulatorModules = ({ simState, ...props }: SimulatorModulesProps) => {
       flexDirection="column"
       height="100%"
       width="100%"
-      p={spacing}
+      py={spacing}
+      px={3}
     >
       {modules.map((id, index) => (
         <CollapsibleModule
@@ -174,7 +175,9 @@ const CollapsibleModule = ({
       <HStack justifyContent="space-between">
         {disclosure.isOpen && (
           <HStack>
-            <Text as="h3">{title}</Text>
+            <Text as="h3" fontWeight="semibold">
+              {title}
+            </Text>
             <IconButton
               aria-label={intl.formatMessage({
                 id: "simulator-reference-link",
