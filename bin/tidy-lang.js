ae09543404ce92b4244cec606dee09987a0bc1b2
Matt Hillsdon <44397098+microbit-matt-hillsdon@users.noreply.github.com>
2022-08-23 16:05:45 +0100

    Separarting sim strings didn't actually work. (#905)
    So let's remove the code and stop having two copies of the strings.
    We might revisit but if we do it needs to start with disjoint files!

--- bin/tidy-lang.js
+++ bin/tidy-lang.js
@@ -6,15 +6,7 @@ const fs = require("fs");
 const path = require("path");
 
 const en = JSON.parse(fs.readFileSync("lang/ui.en.json"));
-// Simulator strings are temporarily separate the from base UI strings
-// that have been frozen for translation.
-const simulatorStrings = JSON.parse(
-  fs.readFileSync("lang/simulator/simulator.en.json")
-);
-const validKeys = new Set([
-  ...Object.keys(en),
-  ...Object.keys(simulatorStrings),
-]);
+const validKeys = new Set(Object.keys(en));
 
 fs.readdirSync("lang")
   .filter((f) => f.endsWith(".json"))
@@ -23,7 +15,6 @@ fs.readdirSync("lang")
     const data = {
       // Ensure we fallback to English even if we haven't roundtripped via Crowdin yet.
       ...en,
-      ...simulatorStrings,
       ...JSON.parse(fs.readFileSync(file)),
     };
     Object.keys(data).forEach((k) => {
