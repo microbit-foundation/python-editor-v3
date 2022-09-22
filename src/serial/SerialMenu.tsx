2a99053edc8cb0ab524a056e2aea43e0a32c6ac4
Robert Knight <robert@microbit.org>
2022-09-22 15:33:21 +0100

    Sim serial help accessibility.

--- src/serial/SerialMenu.tsx
+++ src/serial/SerialMenu.tsx
@@ -12,6 +12,7 @@ import {
   MenuList,
   Portal,
 } from "@chakra-ui/react";
+import React, { ForwardedRef } from "react";
 import { MdMoreVert } from "react-icons/md";
 import { RiInformationLine, RiKeyboardBoxLine } from "react-icons/ri";
 import { FormattedMessage, useIntl } from "react-intl";
@@ -27,46 +28,48 @@ interface SerialMenuProps {
 /**
  * Serial ara drop-down menu.
  */
-const SerialMenu = ({
-  compact,
-  onSizeChange,
-  onShowHintsAndTips,
-}: SerialMenuProps) => {
-  const intl = useIntl();
-  const actions = useSerialActions(onSizeChange);
-  return (
-    <Menu placement={compact ? "top-start" : undefined}>
-      <MenuButton
-        as={IconButton}
-        aria-label={intl.formatMessage({ id: "serial-menu" })}
-        variant="sidebar"
-        color="white"
-        isRound
-        icon={<MdMoreVert />}
-      />
-      <Portal>
-        <MenuList zIndex={zIndexAboveTerminal}>
-          <MenuItem icon={<RiKeyboardBoxLine />} onClick={actions.interrupt}>
-            <FormattedMessage id="serial-ctrl-c-action" />
-          </MenuItem>
-          <MenuItem icon={<RiKeyboardBoxLine />} onClick={actions.reset}>
-            <FormattedMessage id="serial-ctrl-d-action" />
-          </MenuItem>
-          {onShowHintsAndTips && (
-            <>
-              <MenuDivider />
-              <MenuItem
-                icon={<RiInformationLine />}
-                onClick={onShowHintsAndTips}
-              >
-                <FormattedMessage id="serial-hints-and-tips" />
-              </MenuItem>
-            </>
-          )}
-        </MenuList>
-      </Portal>
-    </Menu>
-  );
-};
+const SerialMenu = React.forwardRef(
+  (
+    { compact, onSizeChange, onShowHintsAndTips }: SerialMenuProps,
+    menuButtonRef: ForwardedRef<HTMLButtonElement>
+  ) => {
+    const intl = useIntl();
+    const actions = useSerialActions(onSizeChange);
+    return (
+      <Menu placement={compact ? "top-start" : undefined}>
+        <MenuButton
+          ref={menuButtonRef}
+          as={IconButton}
+          aria-label={intl.formatMessage({ id: "serial-menu" })}
+          variant="sidebar"
+          color="white"
+          isRound
+          icon={<MdMoreVert />}
+        />
+        <Portal>
+          <MenuList zIndex={zIndexAboveTerminal}>
+            <MenuItem icon={<RiKeyboardBoxLine />} onClick={actions.interrupt}>
+              <FormattedMessage id="serial-ctrl-c-action" />
+            </MenuItem>
+            <MenuItem icon={<RiKeyboardBoxLine />} onClick={actions.reset}>
+              <FormattedMessage id="serial-ctrl-d-action" />
+            </MenuItem>
+            {onShowHintsAndTips && (
+              <>
+                <MenuDivider />
+                <MenuItem
+                  icon={<RiInformationLine />}
+                  onClick={onShowHintsAndTips}
+                >
+                  <FormattedMessage id="serial-hints-and-tips" />
+                </MenuItem>
+              </>
+            )}
+          </MenuList>
+        </Portal>
+      </Menu>
+    );
+  }
+);
 
 export default SerialMenu;
