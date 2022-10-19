fe046f4a209b9b7f12739e4a12d1886688bc0edc
Matt Hillsdon <matt.hillsdon@microbit.org>
2022-10-19 11:45:26 +0100

    Migration regression test.

--- src/e2e/migration.test.ts
+++ src/e2e/migration.test.ts
@@ -5,10 +5,15 @@
  */
 import { App } from "./app";
 
+const heartMigrationFragment =
+  "#project:XQAAgACRAAAAAAAAAAA9iImmlGSt1R++5LD+ZJ36cRz46B+lhYtNRoWF0nijpaVyZlK7ACfSpeoQpgfk21st4ty06R4PEOM4sSAXBT95G3en+tghrYmE+YJp6EiYgzA9ThKkyShWq2UdvmCzqxoNfYc1wlmTqlNv/Piaz3WoSe3flvr/ItyLl0aolQlEpv4LA8A=";
+
+const sunlightSensorMigrationFragment =
+  "#project:XQAAgAByAQAAAAAAAAA9iImmlGSt1R++5LD+ZJ36cRz46B+lhYtNRoWF0nijpaVyZlK7ACfSpeoQpgfk21st4ty06R4PEOW6kOsIEMK7SL0Qco7jgsHFKZXfjv/XcHWvXG9qyz1a/a3NUulFDj/FDJxVAIV+WZLpRoo4E6MbW70FOgIfBPWP2hDVsojpoLc7ZfKI8SHxv54FSfB5bkbzaAKO+8CO73t6Odtv691JGjJ9MExFighY6GxyM/DoNInDDpAjFeaqCWrYdwENX7ZVM3we8f4swI71tL28N7sg588aB//A78AA";
+
 describe("Browser - migration", () => {
   const app = new App({
-    fragment:
-      "#project:XQAAgACRAAAAAAAAAAA9iImmlGSt1R++5LD+ZJ36cRz46B+lhYtNRoWF0nijpaVyZlK7ACfSpeoQpgfk21st4ty06R4PEOM4sSAXBT95G3en+tghrYmE+YJp6EiYgzA9ThKkyShWq2UdvmCzqxoNfYc1wlmTqlNv/Piaz3WoSe3flvr/ItyLl0aolQlEpv4LA8A=",
+    fragment: heartMigrationFragment,
   });
   beforeEach(app.reset.bind(app));
   afterEach(app.screenshot.bind(app));
@@ -19,5 +24,13 @@ describe("Browser - migration", () => {
     await app.findVisibleEditorContents(
       "from microbit import *\ndisplay.show(Image.HEART)"
     );
+
+    // Regression test: Check that we can switch to a different migration in the same session.
+    // Previously we ignored the migration because we already had content in session storage.
+    app.setOptions({
+      fragment: sunlightSensorMigrationFragment,
+    });
+    await app.gotoOptionsUrl();
+    await app.findVisibleEditorContents("display.read_light_level");
   });
 });
