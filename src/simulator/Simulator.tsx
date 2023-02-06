ab35c047fa9d9ded31e34a90b0eb71ba8278bad5
Robert Knight <robert@microbit.org>
2023-02-06 16:29:58 +0000

    Unused vars

--- src/simulator/Simulator.tsx
+++ src/simulator/Simulator.tsx
@@ -44,10 +44,10 @@ const Simulator = ({
   minWidth,
   simFocus,
 }: SimulatorProps) => {
-  const production =
-    "https://python-simulator.usermbit.org/v/0.1/simulator.html";
-  const staging =
-    "https://python-simulator.usermbit.org/staging/simulator.html";
+  // const production =
+  //   "https://python-simulator.usermbit.org/v/0.1/simulator.html";
+  // const staging =
+  //   "https://python-simulator.usermbit.org/staging/simulator.html";
   // const url = stage === "PRODUCTION" ? production : staging;
   // For testing with sim branches:
   const branch = "safari-fix";
