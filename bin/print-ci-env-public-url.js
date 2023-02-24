b72526992bb240b3e5994e4c46a5c07162273fe3
Matt Hillsdon <matt.hillsdon@microbit.org>
2023-02-24 16:26:48 +0000

    Fix to avoid deployment.js on forks.
    This version actually builds.

--- bin/print-ci-env-public-url.js
+++ bin/print-ci-env-public-url.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-// STAGE must be defined before this is imported
-const { bucketPrefix } = require("../deployment");
-
+let url;
+if (process.env.GITHUB_REPOSITORY_OWNER === "microbit-foundation") {
+  // STAGE must be defined before this is imported
+  const { bucketPrefix } = require("../deployment");
+  url = `/${bucketPrefix}/`;
+} else {
+  url = "/";
+}
 // Two env vars as PUBLIC_URL seems to be blank when running jest even if we set it.
-console.log(`PUBLIC_URL=/${bucketPrefix}/`);
-console.log(`E2E_PUBLIC_URL=/${bucketPrefix}/`);
+console.log(`PUBLIC_URL=${url}`);
+console.log(`E2E_PUBLIC_URL=${url}`);
