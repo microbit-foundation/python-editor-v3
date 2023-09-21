ae8a5f50d00ed073b085907de7f860ebd75a35a1
Matt Hillsdon <matt.hillsdon@microbit.org>
2023-09-21 11:50:50 +0100

    Upgrade to Node 20
    Drop webusb dep that's only used for some test we largely
    didn't complete. Replace the working test with a mock as its
    use of the webusb dep wasn't meaningful.

--- src/device/webusb.test.ts
+++ src/device/webusb.test.ts
@@ -10,9 +10,15 @@
  * with a tweak to Buffer.
  */
 import { ConnectionStatus, EVENT_STATUS } from "./device";
-import { USB } from "webusb";
 import { NullLogging } from "../deployment/default/logging";
 import { MicrobitWebUSBConnection } from "./webusb";
+
+jest.mock("./dap-wrapper", () => ({
+  DAPWrapper: class DapWrapper {
+    startSerial = jest.fn().mockReturnValue(Promise.resolve());
+    reconnectAsync = jest.fn();
+  },
+}));
 
 const describeDeviceOnly = process.env.TEST_MODE_DEVICE
   ? describe
@@ -30,9 +36,17 @@ describe("MicrobitWebUSBConnection (WebUSB unsupported)", () => {
 
 describeDeviceOnly("MicrobitWebUSBConnection (WebUSB supported)", () => {
   beforeAll(() => {
+    const usb = {
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      requestDevice() {
+        const device = {};
+        return device;
+      },
+    };
     // Maybe we can move this to a custom jest environment?
     (global as any).navigator = {
-      usb: new USB({}),
+      usb,
     };
   });
 
@@ -64,33 +78,4 @@ describeDeviceOnly("MicrobitWebUSBConnection (WebUSB supported)", () => {
       ConnectionStatus.NOT_CONNECTED,
     ]);
   });
-
-  it("flashes using partial flashing when possible", async () => {
-    // Flash MakeCode hex that outputs serial.
-    // Flash MicroPython hex that outputs serial.
-    // Flash another MicroPython hex, assert that we got a small number of progress events.
-    // I think we need to rejig the interface to get flash data before writing this.
-  });
-
-  it("connects to flash and stays connected afterwards", () => {
-    // Is this actually sensible?
-  });
-
-  it("fires a serial reset event when stopping serial", () => {});
-
-  it("reinstates serial after flashing", () => {});
-
-  it("serial echo test", async () => {
-    // Flash a hex that echos serial
-    // Use serialWrite to write serial data.
-    // Assert same content is received.
-    // Useful to test unicode?
-  });
-
-  it("notices disconnects and updates status", () => {
-    // Might need a mock-style tests for this.
-  });
-
-  // Is it feasible to test many of the error cases?
-  // I think we'd need to pass a mock DAPWrapper.
 });
