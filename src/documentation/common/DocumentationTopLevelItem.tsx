b3ee6f8685e6084ef7732f1fe7a32f8011a52e42
Matt Hillsdon <matt.hillsdon@microbit.org>
2023-12-12 14:56:57 +0000

    Tweak colour

--- src/documentation/common/DocumentationTopLevelItem.tsx
+++ src/documentation/common/DocumentationTopLevelItem.tsx
@@ -65,7 +65,7 @@ const DocumentationTopLevelItem = ({
               { name }
             )}
             size="sm"
-            color="brand1.200"
+            color="brand1.300"
             variant="ghost"
             fontSize="2xl"
           />
