d3d0a001fba666afb78beedb63df9cf8f3222ede
wilsonkooi <160124853+wilsonkooi@users.noreply.github.com>
2024-03-03 17:51:29 +0000

    begin parsing arguments in SoundEffect

--- src/editor/codemirror/reactWidgetExtension.tsx
+++ src/editor/codemirror/reactWidgetExtension.tsx
@@ -71,24 +71,53 @@ export const reactWidgetExtension = (
     let from = 0
     let to = state.doc.length-1 // TODO: could optimize this to just be lines within view
     let t = state.doc.toString()
-    let sound = false
-    console.log(t);
+    //console.log(t);
+
+    let sound = false // detected a SoundEffect, waiting to pair with ArgList
+    let parsingArgs = false; let argEnd = 0; // Found pattern to create widget, parse args
+    // I: parenthesis = # open paren - # closed paren, G: parenthesis <= 0
+    // !G ^ I -> # open paren - # closed paren = 0, reached end of ArgList 
+    let parenthesis = 0; 
+
     syntaxTree(state).iterate({
       from, to,
       enter: (node: any) => {
         //console.log(node.name)
         //console.log(state.doc.sliceString(node.from, node.to))
-        if(sound && node.name == "ArgList"){
-          //let isTrue = state.doc.sliceString(node.from, node.to) == "true"
-          let deco = Decoration.widget({
-            widget: new ExampleReactBlockWidget(createPortal),
-            side: 1,
-          });
-          widgets.push(deco.range(node.to))
+
+        // Walking through ArgList, trying to match with values
+        if(parsingArgs){
+          if(node.name == "(") parenthesis += 1
+          else if(node.name == ")") {
+            parenthesis -= 1
+            if(parenthesis <= 0){
+              // finished parsing ArgList
+              parsingArgs = false
+    
+              // create the widget
+              let deco = Decoration.widget({
+                widget: new ExampleReactBlockWidget(createPortal),
+                side: 1,
+              });
+              widgets.push(deco.range(argEnd))
+            }
+          }
+          else{
+            console.log(node.name)
+            console.log(state.doc.sliceString(node.from, node.to))
+            // Claim: if parenthesis = 1 (and code is well formatted) then we are not nested 
+            // Idea: parse through, adding name and slices until a comma is spotted at parenthesis = 1
+            // at the very end, we can figure out if they are valid, and therefore if to modify the visual
+          }
         }
-        // detected SoundEffect, if next expression is an ArgList, show UI
-        // TODO: ensure this is the only case of SoundEffect ArgList
-        sound = node.name == "VariableName" && state.doc.sliceString(node.from, node.to) == "SoundEffect"
+        else{
+          // Found ArgList, will begin to parse nodes 
+          if(sound && node.name == "ArgList") { sound = false; parsingArgs = true; argEnd = node.to }
+
+          // detected SoundEffect, if next expression is an ArgList, show UI
+          // TODO: ensure this is the only case of SoundEffect ArgList
+          sound = node.name == "VariableName" && state.doc.sliceString(node.from, node.to) == "SoundEffect"
+        }
       }
     })
     return Decoration.set(widgets)
