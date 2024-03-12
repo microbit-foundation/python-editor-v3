49a4117b08eb61685c10093fc63692af01f7faec
Matt Hillsdon <matt.hillsdon@microbit.org>
2024-03-12 13:05:01 +0000

    Actually fix script

--- bin/print-ci-env-public-url.cjs
+++ bin/print-ci-env-public-url.cjs
@@ -4,13 +4,14 @@ if (process.env.GITHUB_REPOSITORY_OWNER === "microbit-foundation") {
   // STAGE must be defined before this is imported
   const { bucketPrefix, bucketName } = require("../deployment.cjs");
   baseUrl = `/${bucketPrefix}/`;
+
+  const fullUrl = `https://${bucketName}${baseUrl}`;
+  // This is used for og:url and similar. Not quite right for review domain but we don't really care.
+  console.log(`VITE_FULL_URL=${fullUrl}`);
 } else {
   baseUrl = "/";
 }
-const fullUrl = `https://${bucketName}${baseUrl}`;
 
 // Two env vars as PUBLIC_URL seems to be blank when running jest even if we set it.
 console.log(`PUBLIC_URL=${baseUrl}`);
 console.log(`E2E_PUBLIC_URL=${baseUrl}`);
-// This is used for og:url and similar. Not quite right for review domain but we don't really care.
-console.log(`VITE_FULL_URL=${fullUrl}`);
