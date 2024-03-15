51504432bf84a757e19b0b59ba1d01ab3b5ae6c2
Matt Hillsdon <matt.hillsdon@microbit.org>
2024-03-15 09:53:33 +0000

    Remove debug

--- src/documentation/search/search.worker.nl.ts
+++ src/documentation/search/search.worker.nl.ts
@@ -5,7 +5,5 @@
  */
 import { SearchWorker } from "./search.worker";
 import languageSupport from "@microbit/lunr-languages/lunr.nl";
-if (!languageSupport) {
-  throw new Error("Whoops!");
-}
+
 new SearchWorker(self as DedicatedWorkerGlobalScope, "nl", languageSupport);
