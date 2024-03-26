7be22fffb212fccffcdd4dc6646ed1b49a707d44
wilsonkooi <160124853+wilsonkooi@users.noreply.github.com>
2024-03-26 09:43:02 -0400

    syntaxNode argument

--- src/editor/codemirror/helper-widgets/argumentParser.tsx
+++ src/editor/codemirror/helper-widgets/argumentParser.tsx
@@ -1,10 +1,10 @@
 import { EditorState } from "@codemirror/state";
-
+import { SyntaxNode } from "@lezer/common";
 // TODO: might move parsing to here once arguments are no longer literals
 // see index.d.ts
 
 // Pre: args are all of type number
-export function numberArgs(state: EditorState, args: any[]): number[] {
+export function numberArgs(state: EditorState, args: any[], node:SyntaxNode): number[] {
     let nums = []
     args.forEach(function (value) {
 
