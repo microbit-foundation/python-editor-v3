ad79203bbf776ff0f24888c0fb3832415c1c0bbf
Grace <145345672+microbit-grace@users.noreply.github.com>
2024-04-17 11:13:00 +0100

    In-context translation (#1169)
    Requires a translate flag.
    
    Foundation builds only so we don't get confusing contributions to our Crowdin from forks that might have made text changes. Open to discussing making the Crowdin project configurable if useful to forks.
    
    When the flag is present, change language id to lol and append CrowdIn JIPT script to header.
    
    Related PRs (now merged)
    Pyright - microbit-foundation/pyright#32
    Stubs - microbit-foundation/micropython-microbit-stubs#100

--- src/language-server/pyright.ts
+++ src/language-server/pyright.ts
@@ -12,7 +12,7 @@ import { baseUrl } from "../base";
 import { createUri, LanguageServerClient } from "./client";
 
 // This is modified by bin/update-pyright.sh
-const workerScriptName = "pyright-main-46e9f54371eb3b42b37c.worker.js";
+const workerScriptName = "pyright-main-fbd1b969112a733566b7.worker.js";
 
 // Very simple cache to avoid React re-creating pointlessly in development.
 let counter = 0;
