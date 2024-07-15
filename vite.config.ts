87a92429ba68e49b1d8f477b73a695bebfce535f
Matt Hillsdon <matt.hillsdon@microbit.org>
2024-07-15 15:52:23 +0100

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -73,9 +73,6 @@ export default defineConfig(({ mode }) => {
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
     mockReset: true,
-    deps: {
-      optimizer: { web: { include: ["@microbit/microbit-connection"] } },
-    },
   };
   const config: UserConfig = {
     base: process.env.BASE_URL ?? "/",
