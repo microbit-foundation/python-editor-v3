378cda26cf99437abfe6a2ff291a715db3e0c0aa
Matt Hillsdon <matt.hillsdon@microbit.org>
2025-04-07 20:45:04 +0100

    Update translations

--- src/language-server/pyright.ts
+++ src/language-server/pyright.ts
@@ -13,7 +13,7 @@ import { createUri, LanguageServerClient } from "./client";
 import { CreateToastFnReturn } from "@chakra-ui/react";
 
 // This is modified by bin/update-pyright.sh
-const workerScriptName = "pyright-main-fead31ca35819bebaf64.worker.js";
+const workerScriptName = "pyright-main-f232297e853bac1019f8.worker.js";
 
 // Very simple cache to avoid React re-creating pointlessly in development.
 let counter = 0;
